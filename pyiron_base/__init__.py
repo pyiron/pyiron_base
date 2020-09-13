7c7aaf49b793e40f09151239138b17cf754a9a92
Jan Janßen <janssen@mpie.de>
2020-09-13 12:17:09 +0200

    Minor fixes

--- pyiron_base/__init__.py
+++ pyiron_base/__init__.py
@@ -8,18 +8,18 @@ from pyiron_base.generic.jedi import fix_ipython_autocomplete
 from pyiron_base.generic.parameters import GenericParameters
 from pyiron_base.generic.template import PyironObject
 from pyiron_base.job.executable import Executable
+from pyiron_base.job.external import Notebook
 from pyiron_base.job.generic import GenericJob
 from pyiron_base.job.interactive import InteractiveBase
-from pyiron_base.job.jobstatus import JobStatus, job_status_successful_lst
+from pyiron_base.job.jobstatus import JobStatus, job_status_successful_lst, job_status_finished_lst, job_status_lst
 from pyiron_base.job.jobtype import JOB_CLASS_DICT, JobType, JobTypeChoice
-from pyiron_base.job.external import Notebook
 from pyiron_base.master.generic import GenericMaster, get_function_from_string
 from pyiron_base.master.parallel import ParallelMaster, JobGenerator
 from pyiron_base.master.serial import SerialMasterBase
 from pyiron_base.project.generic import Project
 from pyiron_base.project.gui import ProjectGUI
 from pyiron_base.pyio.parser import Logstatus, extract_data_from_file
-from pyiron_base.server.queuestatus import _validate_que_request
+from pyiron_base.server.queuestatus import validate_que_request
 from pyiron_base.settings.generic import Settings
 from pyiron_base.settings.install import install_dialog
 from ._version import get_versions
