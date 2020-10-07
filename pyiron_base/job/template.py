55b02e43bc01f9b83a506e2b00fcf80f9493ac0c
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2020-10-07 19:43:48 +0200

    Added output based on Inputlist and use save_output to hide hdf5 usage

--- pyiron_base/job/template.py
+++ pyiron_base/job/template.py
@@ -3,6 +3,7 @@
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 from pyiron_base.job.generic import GenericJob
 from pyiron_base.generic.parameters import GenericParameters
+from pyiron_base.generic.inputlist import InputList
 
 """
 Template class to define jobs
@@ -45,7 +46,29 @@ class TemplateJob(GenericJob):
 class PythonTemplateJob(TemplateJob):
     def __init__(self, project, job_name):
         super().__init__(project, job_name)
+        self.output = InputList(table_name="output")
         self._python_only_job = True
+
+    def from_hdf(self, hdf=None, group_name=None):
+        super().from_hdf(
+            hdf=hdf,
+            group_name=group_name
+        )
+        if hdf is None:
+            hdf = self.project_hdf5
+        self.output.from_hdf(hdf=hdf, group_name=None)
+
+    def to_hdf(self, hdf=None, group_name=None):
+        super().to_hdf(
+            hdf=hdf,
+            group_name=group_name
+        )
+        if hdf is None:
+            hdf = self.project_hdf5
+        self.output.to_hdf(hdf=hdf, group_name=None)
+
+    def save_output(self):
+        self.output.to_hdf(hdf=self.project_hdf5, group_name=None)
 
     def _check_if_input_should_be_written(self):
         return False
