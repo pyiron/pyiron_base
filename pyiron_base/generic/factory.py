b347dbc290fb20f6ce33d6cbb718469492379afd
Liam Huber <liam.huber@gmail.com>
2020-11-20 11:55:20 +0100

    Initial commit

---
+++ pyiron_base/generic/factory.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+"""
+Abstract classes for factories that instantiate other pyiron objects
+"""
+
+__author__ = "Liam Huber"
+__copyright__ = (
+    "Copyright 2020, Max-Planck-Institut für Eisenforschung GmbH - "
+    "Computational Materials Design (CM) Department"
+)
+__version__ = "0.1"
+__maintainer__ = "Liam Huber"
+__email__ = "huber@mpie.de"
+__status__ = "development"
+__date__ = "Nov 20, 2020"
+
