2b9ea6e469687023b942653d587f7acdb753a472
Liam Huber <liam.huber@gmail.com>
2020-11-20 12:51:37 +0100

    English

--- pyiron_base/generic/factory.py
+++ pyiron_base/generic/factory.py
@@ -3,7 +3,7 @@
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
 """
-Abstract classes for factories that instantiate other pyiron objects
+Abstract classes for factories that instantiate other pyiron objects.
 """
 
 from abc import ABC
