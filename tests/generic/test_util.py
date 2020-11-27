5adaf641ff00d7d43643327f07a3e4d7f998618b
Marvin Poul <ponder@creshal.de>
2020-11-27 14:55:55 +0100

    Add decorator for deprecation warnings

--- tests/generic/test_util.py
+++ tests/generic/test_util.py
@@ -3,7 +3,9 @@
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
 import unittest
+import warnings
 from pyiron_base.generic.util import static_isinstance
+from pyiron_base.generic.util import Deprecator, deprecate
 
 
 class TestJobType(unittest.TestCase):
@@ -23,6 +25,35 @@ class TestJobType(unittest.TestCase):
         )
         self.assertRaises(TypeError, static_isinstance, list(), 1)
 
+    def test_deprecate(self):
+        """
+        Function decorated with `deprecate` should raise a warning.
+        """
+        @deprecate
+        def foo(a):
+            return 2*a
+
+        @deprecate("use baz instead", version="0.2.0")
+        def bar(a):
+            return 4*a
+
+        with warnings.catch_warnings(record=True) as w:
+            self.assertEqual(foo(1), 2,
+                             "Decorated function does not return original "
+                             "return value")
+        self.assertTrue(len(w) > 0, "No warning raised!")
+        self.assertEqual(w[0].category, DeprecationWarning,
+                        "Raised warning is not a DeprecationWarning")
+
+        with warnings.catch_warnings(record=True) as w:
+            self.assertEqual(bar(1), 4,
+                             "Decorated function does not return original "
+                             "return value")
+
+        expected_message = "use baz instead. It is not guaranteed to be in " \
+                           "service in vers. 0.2.0"
+        self.assertTrue( w[0].message.args[0].endswith(expected_message),
+                        "Warning message does not reflect decorator arguments.")
 
 if __name__ == "__main__":
     unittest.main()
