52271fc1fcf1fc13fe3c21fd5e8b1d2a50361392
muh-hassani <mhassani86@gmail.com>
2021-01-06 22:43:40 +0100

    test_export.py: solving the issue with the paths

--- tests/archiving/test_export.py
+++ tests/archiving/test_export.py
@@ -31,14 +31,14 @@ class TestPacking(unittest.TestCase):
         cls.job = cls.pr.create_job(job_type=ToyJob, job_name='toy')
         cls.job.run()
         cls.pr.packing(destination_path=cls.arch_path,compress=False)
-    
+        cls.file_location = os.path.dirname(os.path.abspath(__file__)).replace(
+            "\\", "/"
+        )
     def test_exportedCSV(self):
         ## in the first test, the csv file from the packing function is read
         ## and is compared with the return dataframe from export_database function   
         self.pr.packing(destination_path=self.arch_path,compress=False)
-        cwd = os.getcwd()
-        csv_file_path = cwd + '/export.csv'
-        df_read = pd.read_csv(csv_file_path)
+        df_read = pd.read_csv('export.csv')
         df_read.drop(df_read.keys()[0],inplace=True,axis = 1)
         df_read.dropna(inplace = True, axis=1) ## this remove the "None/NaN/empty" cells as well as the unnamed column
         df_read['timestart'] = pd.to_datetime(df_read['timestart'])
@@ -48,10 +48,9 @@ class TestPacking(unittest.TestCase):
         assert_frame_equal(df_exp,df_read)
         ## In the second test, an examplary.csv file is read and compared with the
         ## one produced by the packing function
-        csv_file_path = cwd + '/exemplary.csv'
+        csv_file_path = self.file_location + '/exemplary.csv'
         df_known = pd.read_csv(csv_file_path)
-        csv_file_path = cwd + '/export.csv'
-        df_read = pd.read_csv(csv_file_path)
+        df_read = pd.read_csv('export.csv')
         df_known.dropna(inplace = True, axis= 1)
         df_known.drop(["timestart","computer"],inplace=True,axis=1)
         df_read.dropna(inplace = True, axis= 1)
@@ -60,10 +59,11 @@ class TestPacking(unittest.TestCase):
     
     def test_HDF5(self):
         ## first we check whether the toy.h5 file exists in the exported directory
-        file_path = self.arch_path+'/'+self.pr.name+"/toy.h5"
-        self.assertTrue(os.path.exists(file_path))
+        h5_file_path = self.arch_path+'/'+self.pr.name+"/toy.h5"
+        self.assertTrue(os.path.exists(h5_file_path))
         ## second test, check whether the sample_hdf.h5 is the same as toy.h5
-        self.assertEqual(os.system("h5diff sample_hdf.h5 {}".format(file_path)),0)
+        sample_path = self.file_location+'/sample_hdf.h5'
+        self.assertEqual(os.system("h5diff {} {}".format(sample_path,h5_file_path)),0)
 
     def test_compress(self):
         ## here we check whether the packing function does the compressibility right
@@ -73,7 +73,10 @@ class TestPacking(unittest.TestCase):
     
     def test_content(self):
         ## here we test the content of the archive_folder and compare it with the content of the project directory
-        compare_obj = dircmp(self.arch_path,self.pr.path)
+        path_to_compare = self.arch_path + "/" + self.pr.name
+        compare_obj = dircmp(path_to_compare,self.pr.path)
+        print("first path: " + self.arch_path)
+        print("2nd path: " + self.pr.path)
         self.assertEqual(len(compare_obj.diff_files),0)
 
 if __name__ == "__main__":
