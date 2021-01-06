2459d73ca490263ef53a2ff0d4c09456aa5fc2ad
muh-hassani <mhassani86@gmail.com>
2021-01-06 01:34:45 +0100

    updating test_export.py

--- tests/archiving/test_export.py
+++ tests/archiving/test_export.py
@@ -12,12 +12,14 @@ class TestPacking(unittest.TestCase):
     
     @classmethod
     def setUpClass(cls):
-        print("set up the class")
+        #print('set up the class)
+        cls.arch_path = 'archive_folder'
+        cls.arch_path_comp = cls.arch_path+'_comp'
         cls.pr = Project('test')
         cls.pr.remove_jobs_silently(recursive=True)
-        cls.job = cls.pr.create_job(job_type=ToyJob, job_name="toy")
+        cls.job = cls.pr.create_job(job_type=ToyJob, job_name='toy')
         cls.job.run()
-        cls.pr.packing(destination_path='archive_folder',compress=False)
+        cls.pr.packing(destination_path=cls.arch_path,compress=False)
     
     def test_exportedCSV(self):
         ## in the first test, the csv file from the packing function is read
@@ -37,20 +39,20 @@ class TestPacking(unittest.TestCase):
     
     def test_HDF5(self):
         ## first we check whether the toy.h5 file exists in the exported directory
-        file_path = "archive_folder/toy.h5"
+        file_path = self.arch_path+"/toy.h5"
         self.assertTrue(os.path.exists(file_path))
         ## second test, check whether the sample_hdf.h5 is the same as toy.h5
         self.assertEqual(os.system("h5diff sample_hdf.h5 archive_folder/toy.h5"),0)
 
     def test_compress(self):
         ## here we check whether the packing function does the compressibility right
-        self.pr.packing(destination_path='archive_folder_comp',compress=True)
-        file_path = 'archive_folder_comp.tar.gz'
+        self.pr.packing(destination_path=self.arch_path_comp,compress=True)
+        file_path = self.arch_path_comp+"tar.gz"
         self.assertTrue(os.path.exists(file_path))
     
     def test_content(self):
         ## here we test the content of the archive_folder and compare it with the content of the project directory
-        compare_obj = dircmp("archive_folder",self.pr.path)
+        compare_obj = dircmp(self.arch_path,self.pr.path)
         self.assertEqual(len(compare_obj.diff_files),0)
 
 if __name__ == "__main__":
