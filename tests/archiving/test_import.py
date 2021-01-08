a41f42366c877f9626b28dd323780ee227aab5b5
muh-hassani <mhassani86@gmail.com>
2021-01-08 02:04:46 +0100

    resolving codacy issues

--- tests/archiving/test_import.py
+++ tests/archiving/test_import.py
@@ -8,11 +8,10 @@ from pyiron_base import PythonTemplateJob
 
 class ToyJob(PythonTemplateJob):
     def __init__(self, project, job_name):
-        '''a toyjob to test export/import functionalities.'''
+        """a toyjob to test export/import functionalities."""
         super(ToyJob, self).__init__(project, job_name)
         self.input['input_energy'] = 100
-    
-    # This function is executed 
+    # This function is executed
     def run_static(self):
         with self.project_hdf5.open("output/generic") as h5out:
             h5out["energy_tot"] = self.input["input_energy"]
