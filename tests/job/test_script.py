6b1fa75ea5a4f0172094c050c5033bc42edf08b5
Liam Huber <liam.huber@gmail.com>
2021-02-19 22:22:10 +0100

    Beautify script job tests

--- tests/job/test_script.py
+++ tests/job/test_script.py
@@ -3,26 +3,25 @@
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
 import unittest
-import os
+from os.path import dirname, abspath, join
 from pyiron_base.project.generic import Project
 
 
 class TestScriptJob(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.file_location = os.path.dirname(os.path.abspath(__file__)).replace(
-            "\\", "/"
-        )
-        cls.project = Project(os.path.join(cls.file_location, "test_sriptjob"))
+        cls.file_location = dirname(abspath(__file__)).replace("\\", "/")
+        cls.project = Project(join(cls.file_location, "test_sriptjob"))
 
     @classmethod
     def tearDownClass(cls):
         cls.project.remove(enable=True)
 
     def test_script_path(self):
-        job = self.project.create_job('ScriptJob', 'script')
+        job = self.project.create.job.ScriptJob('script')
         with self.assertRaises(TypeError):
             job.run()
+
 
 if __name__ == "__main__":
     unittest.main()
