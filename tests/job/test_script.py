dde54e0a0bf829cc1beea9cedd98ece01d7b73cf
Liam Huber <liam.huber@gmail.com>
2021-02-19 22:35:19 +0100

    Add a script job test to check new data functionality

--- tests/job/test_script.py
+++ tests/job/test_script.py
@@ -4,6 +4,7 @@
 
 import unittest
 from os.path import dirname, abspath, join
+from os import remove
 from pyiron_base.project.generic import Project
 
 
@@ -11,16 +12,45 @@ class TestScriptJob(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.file_location = dirname(abspath(__file__)).replace("\\", "/")
-        cls.project = Project(join(cls.file_location, "test_sriptjob"))
+        cls.project_name = join(cls.file_location, "test_sriptjob")
+        cls.project = Project(cls.project_name)
+        cls.simple_script = join(cls.file_location, 'simple.py')
+        cls.complex_script = join(cls.file_location, 'complex.py')
 
     @classmethod
     def tearDownClass(cls):
         cls.project.remove(enable=True)
+        remove(cls.simple_script)
+        remove(cls.complex_script)
+
+    def setUp(self):
+        self.job = self.project.create.job.ScriptJob('script')
+
+    def tearDown(self):
+        self.project.remove_job('script')
 
     def test_script_path(self):
-        job = self.project.create.job.ScriptJob('script')
         with self.assertRaises(TypeError):
-            job.run()
+            self.job.run()
+
+        with open(self.simple_script, 'w') as f:
+            f.write("print(42)")
+        self.job.script_path = self.simple_script
+        self.job.run()
+
+    def test_project_data(self):
+        self.project.data.in_ = 6
+        self.project.data.write()
+        with open(self.complex_script, 'w') as f:
+            f.write("from pyiron_base import Project\n")
+            f.write(f"pr = Project('{self.project_name}')\n")
+            f.write("pr.data.read()\n")  # TODO: Remove this line once data is auto-reading
+            f.write("pr.data.out = pr.data.in_ * 7\n")
+            f.write("pr.data.write()\n")
+        self.job.script_path = self.complex_script
+        self.job.run()
+        self.project.data.read()
+        self.assertEqual(42, self.project.data.out)
 
 
 if __name__ == "__main__":
