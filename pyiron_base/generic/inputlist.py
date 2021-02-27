ec74da1f16b404cb4b0836642fa4ffd5dc87a9a2
Marvin Poul <ponder@creshal.de>
2021-02-27 12:00:59 +0100

    Add deprecation warning to InputList

--- pyiron_base/generic/inputlist.py
+++ pyiron_base/generic/inputlist.py
@@ -2,4 +2,14 @@
 Backwards compatible way of importing the DataContainer.
 """
 
-from .datacontainer import DataContainer as InputList
+from .datacontainer import DataContainer
+from .util import deprecate
+
+from functools import wraps
+
+class InputList(DataContainer):
+
+    @deprecate("use DataContainer instead", version="0.3.0")
+    @wraps(DataContainer.__init__)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
