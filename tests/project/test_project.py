ff080ef6bee8e46d9e649670f81ae3a399c5d3b1
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-03-16 21:22:44 +0100

    Add tests

--- tests/project/test_project.py
+++ tests/project/test_project.py
@@ -6,7 +6,9 @@ import unittest
 from os.path import dirname, join, abspath
 from os import remove
 from pyiron_base.project.generic import Project
+from pyiron_base.project.project_browser import ProjectBrowser
 from tests.toy_job_run import ToyJob
+import ipywidgets as widgets
 
 
 class TestProjectData(unittest.TestCase):
@@ -67,6 +69,9 @@ class TestProjectBrowser(unittest.TestCase):
         except FileNotFoundError:
             pass
 
+    def setUp(self):
+        self.browser = self.project.browser.copy()
+
     def test_init_browser(self):
         self.assertTrue(self.project.browser.project is self.project)
         self.assertEqual(self.project.browser.path, self.project.path)
@@ -74,6 +79,11 @@ class TestProjectBrowser(unittest.TestCase):
         self.assertTrue(self.project.browser.hide_path)
         self.assertFalse(self.project.browser.fix_path)
         self.assertTrue(self.project.browser._node_as_dirs)
+
+        vbox = widgets.VBox()
+        browser = ProjectBrowser(project=self.project, Vbox=vbox)
+        self.assertTrue(browser.box is vbox and browser.project is self.project)
+        self.assertTrue(len(browser.box.children) > 0)
 
     def test_copy(self):
         browser = self.project.browser.copy()
@@ -83,7 +93,8 @@ class TestProjectBrowser(unittest.TestCase):
         self.assertEqual(browser.fix_path, self.project.browser.fix_path)
 
     def test_configure(self):
-        browser = self.project.browser.copy()
+        browser = self.browser
+        vbox = widgets.VBox()
 
         browser.configure(show_files=True)
         self.assertTrue(browser.show_files)
@@ -105,10 +116,11 @@ class TestProjectBrowser(unittest.TestCase):
         self.assertFalse(browser.hide_path)
         self.assertTrue(browser.fix_path)
 
-        browser.configure(show_files=False, fix_path=False, hide_path=True)
-        self.assertFalse(self.project.browser.show_files)
-        self.assertTrue(self.project.browser.hide_path)
-        self.assertFalse(self.project.browser.fix_path)
+        browser.configure(show_files=False, fix_path=False, hide_path=True, Vbox=vbox)
+        self.assertFalse(browser.show_files)
+        self.assertTrue(browser.hide_path)
+        self.assertFalse(browser.fix_path)
+        self.assertTrue(browser.box is vbox)
 
     def test_files(self):
         browser = self.project.browser.copy()
@@ -129,8 +141,11 @@ class TestProjectBrowser(unittest.TestCase):
         browser = self.project.browser.copy()
         self.assertEqual(browser._clickedFiles, [])
         browser._on_click_file('text.txt')
+        browser.refresh()
         self.assertEqual(browser._clickedFiles, [join(browser.path, 'text.txt')])
         self.assertEqual(browser.data.data, ["some text"])
+        browser._on_click_file('text.txt')
+        self.assertTrue(browser.data is None)
 
     def test__update_project(self):
         browser = self.project.browser.copy()
@@ -138,6 +153,72 @@ class TestProjectBrowser(unittest.TestCase):
         browser._update_project(path)
         self.assertIsInstance(browser.project, ToyJob)
         self.assertFalse(browser._node_as_dirs)
+        self.assertEqual(browser.path, path)
+
+        browser._on_click_file('text.txt')
+        self.assertTrue(browser.data is None)
+
+        browser._update_project(path)
+        self.assertEqual(browser.path, path)
+
+        browser._update_project(self.project)
+        self.assertEqual(browser.path, self.project.path)
+        self.assertIs(browser.project, self.project)
+
+        browser._update_project("NotExistingPath")
+        self.assertEqual(browser.path, self.project.path)
+        self.assertIs(browser.project, self.project)
+
+    def test__busy_check(self):
+        self.assertFalse(self.browser._busy_check())
+        self.assertTrue(self.browser._busy_check())
+        self.assertFalse(self.browser._busy_check(busy=False))
+
+    def test_gui(self):
+        self.browser.gui()
+
+    def test_box(self):
+        Vbox = widgets.VBox()
+        self.browser.box = Vbox
+        self.assertTrue(self.browser.box is Vbox)
+        self.assertTrue(len(self.browser.box.children) > 0)
+
+    def test_fix_path(self):
+        self.assertFalse(self.browser.fix_path)
+        self.browser.fix_path = True
+        self.assertTrue(self.browser.fix_path)
+
+    def test_hide_path(self):
+        self.assertTrue(self.browser.hide_path)
+        self.browser.hide_path = False
+        self.assertFalse(self.browser.hide_path)
+
+    def test__click_option_button(self):
+        reset_button = widgets.Button(description="Reset selection")
+        set_path_button = widgets.Button(description="Set Path")
+
+        self.browser._on_click_file('text.txt')
+        self.browser._click_option_button(reset_button)
+        self.assertIs(self.browser.data, None)
+        self.assertEqual(self.browser._clickedFiles, [])
+
+        self.browser._click_option_button(set_path_button)
+
+        self.browser.fix_path = True
+        self.browser.path_string_box.value = "sub"
+        self.browser._click_option_button(set_path_button)
+        self.assertEqual(self.browser.path, self.project.path)
+
+        self.browser.fix_path = False
+        self.browser.path_string_box.value = "sub"
+        self.browser._click_option_button(set_path_button)
+        self.assertEqual(self.browser.path, join(self.project.path, 'sub/'))
+        self.assertEqual(self.browser.path_string_box.value, "")
+
+        self.browser.path_string_box.value = self.project.path
+        self.browser._click_option_button(set_path_button)
+        self.assertEqual(self.browser.path, self.project.path)
+        self.assertEqual(self.browser.path_string_box.value, "")
 
 
 if __name__ == '__main__':
