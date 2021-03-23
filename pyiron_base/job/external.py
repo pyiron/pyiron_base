99124150614c5a1d5abb87db0680248e45950a87
Jan Janßen <janssen@mpie.de>
2021-03-23 15:42:10 +0100

    Fix ScriptJob

--- pyiron_base/job/external.py
+++ pyiron_base/job/external.py
@@ -9,6 +9,7 @@ import json
 from pathlib2 import Path
 import warnings
 from pyiron_base.generic.hdfio import FileHDFio
+from pyiron_base.generic.datacontainer import DataContainer
 
 __author__ = "Osamu Waseda"
 __copyright__ = (
@@ -36,10 +37,13 @@ class Notebook(object):
         hdf_file = project_folder / folder
         hdf_file = str(hdf_file) + ".h5"
         if Path(hdf_file).exists():
-            hdf = FileHDFio(hdf_file)
-            custom_dict = hdf[folder + '/input/custom_dict/data']
-            custom_dict["project_dir"] = str(project_folder)
-            return custom_dict
+            obj = DataContainer()
+            obj.from_hdf(
+                hdf = FileHDFio(hdf_file),
+                group_name = folder + '/input/custom_dict'
+            )
+            obj["project_dir"] = str(project_folder)
+            return obj
         elif Path("input.json").exists():
             with open("input.json") as f:
                 return json.load(f)
