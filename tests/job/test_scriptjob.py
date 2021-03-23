791b34a097e71e8b8f7833b2936cb709dbaedbae
Liam Huber <liam.huber@gmail.com>
2021-03-23 19:43:48 +0100

    update scriptjob tests

--- tests/job/test_scriptjob.py
+++ tests/job/test_scriptjob.py
@@ -1,22 +1,11 @@
-import os
-import unittest
+from pyiron_base._tests import TestWithProject
 
-from pyiron_base import Project
 
-class TestScriptJob(unittest.TestCase):
+class TestScriptJob(TestWithProject):
     @classmethod
     def setUpClass(cls):
-        cls.file_location = os.path.dirname(os.path.abspath(__file__)).replace(
-            "\\", "/"
-        )
-        cls.project = Project(os.path.join(cls.file_location, "test_scriptjob"))
-        cls.job = cls.project.create_job(cls.project.job_type.ScriptJob,
-                                               "test")
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.project.remove(enable=True)
-
+        super().setUpClass()
+        cls.job = cls.project.create.job.ScriptJob("test")
 
     def test_notebook_input(self):
         """
@@ -27,4 +16,4 @@ class TestScriptJob(unittest.TestCase):
         self.job.input['value'] = 300
         self.job.save()
         self.assertTrue("custom_dict" in self.job["input"].list_groups(),
-                        "Input not saved in the 'custom_dict' group in HDF")
+                        msg="Input not saved in the 'custom_dict' group in HDF")
