954d8e077000c1ccca047e7d0c80d55164cb9d0f
Liam Huber <liam.huber@gmail.com>
2021-03-23 13:24:31 +0100

    Srsly

--- pyiron_base/_tests.py
+++ pyiron_base/_tests.py
@@ -23,9 +23,10 @@ __date__ = "Mar 23, 2021"
 
 
 class TestWithProject(unittest.TestCase, ABC):
-
     """
     Tests that start and remove a project for their suite, and remove jobs from the project for each test.
+
+    Now this is not a one-line docstring.
     """
 
     @classmethod
