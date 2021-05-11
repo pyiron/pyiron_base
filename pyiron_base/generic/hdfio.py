938a2e8ded7862b199051b72f203b8b34f441a9b
Marvin Poul <ponder@creshal.de>
2021-05-11 11:31:17 +0200

    Always add HDF5 file extension to path

--- pyiron_base/generic/hdfio.py
+++ pyiron_base/generic/hdfio.py
@@ -111,6 +111,8 @@ class FileHDFio(object):
     """
 
     def __init__(self, file_name, h5_path="/", mode="a"):
+        if not file_name.endswith(".h5"):
+            file_name += ".h5"
         if not os.path.isabs(file_name):
             raise ValueError("file_name must be given as absolute path name")
         self._file_name = None
@@ -1045,9 +1047,9 @@ class ProjectHDFio(FileHDFio):
     """
 
     def __init__(self, project, file_name, h5_path=None, mode=None):
+        if not file_name.endswith(".h5"):
+            file_name += ".h5"
         self._file_name = file_name.replace("\\", "/")
-        if ".h5" not in file_name:
-            self._file_name += ".h5"
         if h5_path is None:
             h5_path = "/"
         self._project = project.copy()
