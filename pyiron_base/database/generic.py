c113319f5c476f58ea6dbd9d2967c1f2d7240f6a
Marvin Poul <ponder@creshal.de>
2021-05-31 12:05:33 +0200

    Add missing settings import

--- pyiron_base/database/generic.py
+++ pyiron_base/database/generic.py
@@ -427,6 +427,9 @@ class DatabaseAccess(IsDatabase):
         if element_lst is not None:
             dict_clause["element_lst"] = element_lst
 
+        # HACK: breaks circular dependency of Settings on DatabaseAccess
+        from pyiron_base.settings.generic import Settings
+        s = Settings()
         s.logger.debug("sql_query: %s", str(dict_clause))
         return self.get_items_dict(dict_clause)
 
