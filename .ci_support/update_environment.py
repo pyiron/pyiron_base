d92b1c9d2bb85c096ff4f80cc95669451c55ac7f
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-07-05 22:06:25 +0200

    :bug: correct sys.argv indexing

--- .ci_support/update_environment.py
+++ .ci_support/update_environment.py
@@ -13,12 +13,13 @@ def convert_package_name(name, name_conv_dict):
     return result
 
 
-if not (sys.argv[0] == 'Bump' and sys.argv[2] == 'from' and sys.argv[4] == 'to'):
-    raise ValueError("Title of a dependabot PR expected")
+if len(sys.argv) != 7 or not (sys.argv[1] == 'Bump' and sys.argv[3] == 'from' and sys.argv[5] == 'to'):
+    raise ValueError(f"Title of a dependabot PR 'Bump <package> from <version> to <version>' expected, "
+                     f"but got {' '.join(sys.argv[1:])}")
 
-package_to_update = sys.argv[1]
-from_version = sys.argv[3]
-to_version = sys.argv[5]
+package_to_update = sys.argv[2]
+from_version = sys.argv[4]
+to_version = sys.argv[6]
 
 with open('.ci_support/pypi_vs_conda_names.json', 'r') as f:
     name_conversion_dict = json.load(f)
