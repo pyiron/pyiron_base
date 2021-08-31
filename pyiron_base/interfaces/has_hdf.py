0da4c88ab5361a336804f49c36dae171bfcb040b
Marvin Poul <ponder@creshal.de>
2021-08-31 11:34:04 +0200

    Add copyright blurb

--- pyiron_base/interfaces/has_hdf.py
+++ pyiron_base/interfaces/has_hdf.py
@@ -1,4 +1,20 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+"""Interface for classes to serialize to HDF5."""
+
 from abc import ABC, abstractmethod
+
+__author__ = "Marvin Poul"
+__copyright__ = (
+    "Copyright 2021, Max-Planck-Institut für Eisenforschung GmbH - "
+    "Computational Materials Design (CM) Department"
+)
+__version__ = "1.0"
+__maintainer__ = "Marvin Poul"
+__email__ = "poul@mpie.de"
+__status__ = "production"
+__date__ = "Sep 1, 2021"
 
 class _WithHDF:
     __slots__ = ("_hdf", "_group_name")
