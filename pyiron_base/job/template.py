c4dce44c719f5d8589003fe589e7e053b1e16bf9
Liam Huber <liam.huber@gmail.com>
2021-09-07 16:25:06 +0200

    Update template job to use DataContainer

--- pyiron_base/job/template.py
+++ pyiron_base/job/template.py
@@ -6,7 +6,7 @@ Template class to define jobs
 """
 
 from pyiron_base.job.generic import GenericJob
-from pyiron_base.generic.parameters import GenericParameters
+from pyiron_base.generic.datacontainer import DataContainer
 
 __author__ = "Jan Janssen"
 __copyright__ = (
@@ -23,23 +23,31 @@ __date__ = "May 15, 2020"
 class TemplateJob(GenericJob):
     def __init__(self, project, job_name):
         super().__init__(project, job_name)
-        self.input = GenericParameters(table_name="input")
+        self._data = DataContainer(table_name="data")
+        self._data.create_group('input')
+        self._data.create_group('output')
+
+    @property
+    def input(self):
+        return self._data.input
+
+    @property
+    def output(self):
+        return self._data.output
 
     def to_hdf(self, hdf=None, group_name=None):
         super().to_hdf(
             hdf=hdf,
             group_name=group_name
         )
-        with self.project_hdf5.open("input") as h5in:
-            self.input.to_hdf(h5in)
+        self._data.to_hdf(hdf=hdf, group_name=group_name)
 
     def from_hdf(self, hdf=None, group_name=None):
         super().from_hdf(
             hdf=hdf,
             group_name=group_name
         )
-        with self.project_hdf5.open("input") as h5in:
-            self.input.from_hdf(h5in)
+        self._data.from_hdf(hdf=hdf, group_name=group_name)
 
 
 class PythonTemplateJob(TemplateJob):
