0ecdfee21fdb5aa5eaa82ebdb09110ac21f83e7f
Liam Huber <liam.huber@gmail.com>
2021-09-07 16:05:29 +0200

    Add tests

---
+++ tests/test_toolkit.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+from pyiron_base._tests import TestWithProject
+from pyiron_base.toolkit import BaseTools
+
+
+class TestToolkit(TestWithProject):
+    def setUp(self):
+        super().setUp()
+        self.tools = BaseTools(self.project)
+
+    def test_job(self):
+        self.tools.job.ScriptJob('foo')
+        with self.assertRaises(AttributeError):
+            self.tools.job.NotAJobAtAll('bar')
