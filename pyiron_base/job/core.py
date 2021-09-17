e6620353dcd24b595e43d2cce3417d1db28c4f81
Raynol Dsouza <dsouza@mpie.de>
2021-09-17 02:17:31 +0200

    check if this fixes test fail

--- pyiron_base/job/core.py
+++ pyiron_base/job/core.py
@@ -663,7 +663,6 @@ class JobCore(HasGroups):
             JobCore: New FileHDFio object pointing to the same HDF5 file
         """
         copied_self = copy.copy(self)
-        copied_self.reset_job_id()
         return copied_self
 
     def _internal_copy_to(self, project=None, new_job_name=None, new_database_entry=True,
