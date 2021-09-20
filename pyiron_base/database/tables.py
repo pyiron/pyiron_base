759da857a2ad6cbe99f9b9ab08cf0132c6ee1bda
Liam Huber <liam.huber@gmail.com>
2021-09-20 18:01:22 +0200

    Initial draft

--- pyiron_base/database/tables.py
+++ pyiron_base/database/tables.py
@@ -46,119 +46,184 @@ class TooLongDBEntry(Exception):
         super(TooLongDBEntry, self).__init__(message)
 
 
+class ColumnManager(ABC):
+    """
+    A parent class for pyiron database columns
+    """
+
+    def __init__(
+            self,
+            column_name,
+            column_type,
+            object_to_entry_fnc,
+            length_limit_is_critical=False,
+            column_kwargs=None,
+            type_kwargs=None
+    ):
+        column_kwargs = {} if column_kwargs is None else column_kwargs
+        type_kwargs = {} if type_kwargs is None else type_kwargs
+        self._name = column_name
+        self._column = Column(column_name, column_type(**type_kwargs), **column_kwargs)
+        self._object_to_entry = object_to_entry_fnc
+        self._length_limit_is_critical = length_limit_is_critical
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def column(self):
+        return self._column
+
+    def object_to_entry(self, obj, settings):
+        entry = self._object_to_entry(obj, settings)
+        return self._check_entry_length(entry)
+
+    def _check_entry_length(self, entry):
+        if hasattr(self.column, 'length') and len(entry) > self.column.length:
+            if self._length_limit_is_critical:
+                raise TooLongDBEntry(
+                    key=self.name,
+                    limit=self.column.length,
+                    val=len(entry)
+                )
+            else:
+                return "OVERFLOW_ERROR"
+        else:
+            return entry
+
+
 class TableManager(ABC):
     """
     A parent class for pyiron database tables.
     """
 
     def __init__(self, table_name, metadata, extend_existing=True):
-        self._char_limit = {}
-        self._update_char_limit()
         self._table = self._create_table(table_name, metadata, extend_existing=extend_existing)
+
+    @property
+    @abstractmethod
+    def _columns(self):
+        """A list of `ColumnManager` objects"""
+        pass
 
     def add(self, obj, settings):
         """Add data from an object to the database table."""
-        par_dict = self._object_to_entry(obj, settings)
-        par_dict = dict(
-            (key.lower(), value) for key, value in par_dict.items()
+        return self._table.insert(
+            {c.name.lower(): c.object_to_entry(obj, settings) for c in self._columns}
         )
-        return self._table.insert(par_dict)
 
     @property
     def table(self) -> Table:
         return self._table
 
-    @abstractmethod
-    def _update_char_limit(self) -> dict:
-        pass
-
     @property
-    @abstractmethod
-    def _restrictively_limited_size_keys(self):
-        pass
+    def columns(self):
+        return [ColumnManager("id", Integer, column_kwargs={'primary_key': True, 'autoincrement': True})] + \
+               self._columns
 
-    @abstractmethod
     def _create_table(self, table_name: str, metadata: MetaData, extend_existing: bool=True) -> Table:
-        """Initialize a `Table` with all the columns you want."""
-        pass
+        return Table(
+            table_name,
+            metadata,
+            *self._columns,
+            extend_existing=extend_existing
+        )
 
-    @abstractmethod
-    def _object_to_entry(self, obj, settings) -> dict:
-        """Parse an object into a database entry."""
-        pass
 
-    @abstractmethod
-    def _check_char_limits(self, par_dict) -> dict:
-        """ checks the character limits of each column"""
-        pass
+def _attribute_or_none_decorator():
+    raise NotImplementedError
 
 
 class HistoricalTable(TableManager):
     """The historical table."""
-    def __init__(self, table_name, metadata, extend_existing=True):
-        super().__init__(table_name, metadata, extend_existing=extend_existing)
-
-    def _update_char_limit(self):
-        self._char_limit.update({'projectpath': 50, 'project': 255, 'job': 50, 'subjob': 250,
-                                 'chemicalformula': 50, 'status': 20, 'hamilton': 50, 'hamversion': 50,
-                                 'username': 20, 'computer': 100})
 
     @property
-    def _restrictively_limited_size_keys(self):
-        return ['projectpath', 'project', 'job', 'subjob', 'hamilton', 'hamversion', 'username', 'computer']
+    def _columns(self):
+        return [
+            ColumnManager("parentid", Integer, self._get_parentid),
+            ColumnManager("masterid", Integer, self._get_masterid),
+            ColumnManager("projectpath", String, self._get_projectpath,
+                          type_kwargs={'length': 50}, length_limit_is_critical=True),
+            ColumnManager("project", String, self._get_project,
+                          type_kwargs={'length': 255}, length_limit_is_critical=True),
+            ColumnManager("job", String, self._get_job,
+                          type_kwargs={'length': 50}, length_limit_is_critical=True),
+            ColumnManager("subjob", String, self._get_subjob,
+                          type_kwargs={'length': 250}, length_limit_is_critical=True),
+            ColumnManager("chemicalformula", String, self._get_chemicalformula,
+                          type_kwargs={'length': 50}),
+            ColumnManager("status", String, self._get_status,
+                          type_kwargs={'length': 20}, length_limit_is_critical=True),
+            ColumnManager("hamilton", String, self._get_hamilton,
+                          type_kwargs={'length': 50}, length_limit_is_critical=True),
+            ColumnManager("hamversion", String, self._get_hamversion,
+                          type_kwargs={'length': 50}, length_limit_is_critical=True),
+            ColumnManager("username", String, self._get_username,
+                          type_kwargs={'length': 20}, length_limit_is_critical=True),
+            ColumnManager("computer", String, self._get_computer,
+                          type_kwargs={'length': 100}, length_limit_is_critical=True),
+            ColumnManager("timestart", self._get_timestart, DateTime),
+            ColumnManager("timestop", self._get_timestop, DateTime),
+            ColumnManager("totalcputime", self._get_totalcputime, Float),
+        ]
 
-    def _create_table(self, table_name, metadata, extend_existing=True):
-        return Table(
-            table_name,
-            metadata,
-            Column("id", Integer, primary_key=True, autoincrement=True),
-            Column("parentid", Integer),
-            Column("masterid", Integer),
-            Column("projectpath", String(self._char_limit["projectpath"])),
-            Column("project", String(self._char_limit["project"])),
-            Column("job", String(self._char_limit["job"])),
-            Column("subjob", String(self._char_limit["subjob"])),
-            Column("chemicalformula", String(self._char_limit["chemicalformula"])),
-            Column("status", String(self._char_limit["status"])),
-            Column("hamilton", String(self._char_limit["hamilton"])),
-            Column("hamversion", String(self._char_limit["hamversion"])),
-            Column("username", String(self._char_limit["username"])),
-            Column("computer", String(self._char_limit["computer"])),
-            Column("timestart", DateTime),
-            Column("timestop", DateTime),
-            Column("totalcputime", Float),
-            extend_existing=extend_existing
-        )
+    @staticmethod
+    def _get_parentid(obj, settings):
+        return obj.parent_id
 
-    def _object_to_entry(self, obj, settings) -> dict:  # : Type[GenericJob] circular import issues
-        par_dict = {
-            "username": settings.login_user,
-            "projectpath": obj.project_hdf5.root_path,
-            "project": obj.project_hdf5.project_path,
-            "job": obj.job_name,
-            "subjob": obj.project_hdf5.h5_path,
-            "hamversion": obj.version,
-            "hamilton": obj.__name__,
-            "status": obj.status.string,
-            "computer": obj._db_server_entry(),
-            "timestart": datetime.now(),
-            "masterid": obj.master_id,
-            "parentid": obj.parent_id,
-            "chemicalformula": obj.chemical_formula
-        }
-        return self._check_char_limits(par_dict)
+    @staticmethod
+    def _get_masterid(obj, settings):
+        return obj.chemical_formula
 
-    def _check_char_limits(self, par_dict):
-        """
-        performs a check whether the length of db entries do not exceed the defined limit
-        args:
-        par_dict(dict): dictionary of the parameter
-        """
-        for key in par_dict.keys():
-            if par_dict[key] is not None and \
-                    len(par_dict[key]) > self._char_limit[key.lower()]:
-                if key.lower() not in self._restrictively_limited_size_keys:
-                    par_dict[key] = "OVERFLOW_ERROR"
-                else:
-                    raise TooLongDBEntry(key=key, limit=self._char_limit[key.lower()], val=par_dict[key])
-        return par_dict
+    @staticmethod
+    def _get_username(obj, settings):
+        return settings.login_user
+
+    @staticmethod
+    def _get_projectpath(obj, settings):
+        return obj.project_hdf5.root_path
+
+    @staticmethod
+    def _get_project(obj, settings):
+        return obj.project_hdf5.project_path
+
+    @staticmethod
+    def _get_job(obj, settings):
+        return obj.job_name
+
+    @staticmethod
+    def _get_subjob(obj, settings):
+        return obj.project_hdf5.
+
+    @staticmethod
+    def _get_chemicalformula(obj, settings):
+        return obj.chemical_formula
+
+    @staticmethod
+    def _get_hamilton(obj, settings):
+        return obj.__name__
+
+    @staticmethod
+    def _get_hamversion(obj, settings):
+        return obj.version
+
+    @staticmethod
+    def _get_status(obj, settings):
+        return obj.status.string
+
+    @staticmethod
+    def _get_computer(obj, settings):
+        return obj._db_server_entry()
+
+    @staticmethod
+    def _get_timestart(obj, settings):
+        return datetime.now()
+
+    @staticmethod
+    def _get_timestop(obj, settings):
+        return obj._runtime()['timestop']
+
+    @staticmethod
+    def _get_totalcputime(obj, settings):
+        return obj._runtime()['totalcputime']
