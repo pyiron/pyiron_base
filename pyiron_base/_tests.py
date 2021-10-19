58a849b296a1904642c56f654fff757b2c25be77
sudarsan1989 <surendralal@mpie.de>
2021-10-19 10:54:51 +0200

    pep8 fix

--- pyiron_base/_tests.py
+++ pyiron_base/_tests.py
@@ -127,5 +127,4 @@ class TestWithFilledProject(TestWithProject, ABC):
             job.status.aborted = True
 
 
-
 _TO_SKIP = [PyironTestCase, TestWithProject, TestWithCleanProject, TestWithFilledProject]
