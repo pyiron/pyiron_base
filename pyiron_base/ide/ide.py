ea14eab20d2d57be3e9aacd0266c8b4b2d7e09aa
liamhuber <liam.huber@gmail.com>
2021-10-26 17:23:47 +0200

    Clean up empty files

--- pyiron_base/ide/ide.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
-# Distributed under the terms of "New BSD License", see the LICENSE file.
-
-
-
-
-
