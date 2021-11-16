65a482cfecb79d83d2b4db06a528d6c48d572d2c
Marvin Poul <ponder@creshal.de>
2021-11-16 18:00:47 +0100

    Add script that rewrites all HDF5 files in a project

---
+++ update_scripts/pyiron_base_0.3_to_0.4.py
@@ -0,0 +1,15 @@
+"""
+pyiron_base<=0.3.9 has a bug that writes all arrays with dtype=object even
+numeric ones.  As a fix pyiron_base=0.3.10 introduces a conversion when reading
+such arrays, but does not automatically save them.  This conversion script
+simply goes over all jobs and rewrites their HDF5 files, since it's read with
+the correct dtype, this then writes this correct dtype.
+"""
+
+import sys
+
+from pyiron_base import Project
+
+pr = Project(sys.argv[1])
+for j in pr.iter_jobs(convert_to_object=False, recursive=True):
+    j.project_hdf5.rewrite_hdf5(j.name)
