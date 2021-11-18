d9b45b96b2b8966694254117565001f0084fd2d4
Marvin Poul <poul@mpie.de>
2021-11-18 11:23:23 +0000

    Fix version number
    Co-authored-by: Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>

--- update_scripts/pyiron_base_0.3_to_0.4.py
+++ update_scripts/pyiron_base_0.3_to_0.4.py
@@ -1,5 +1,5 @@
 """
-pyiron_base<=0.3.9 has a bug that writes all arrays with dtype=object even
+pyiron_base<=0.3.10 has a bug that writes all arrays with dtype=object even
 numeric ones.  As a fix pyiron_base=0.4.0 introduces a conversion when reading
 such arrays, but does not automatically save them.  This conversion script
 simply goes over all jobs and rewrites their HDF5 files, since it's read with
