7e148e56b1391adc8a576aef92a8c1705b47e5c8
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-11-17 21:16:03 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         'pyfileindex==0.0.6',
         'pysqa==0.0.15',
         'sqlalchemy==1.4.26',
-        'tables==3.6.1',
         'tqdm==4.62.3'
     ],
     cmdclass=versioneer.get_cmdclass(),
