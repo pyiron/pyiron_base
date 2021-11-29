aee17d919ef84eb57ac6e94687c8bd37d5577968
Liam Huber <liam.huber@gmail.com>
2021-11-29 17:44:33 +0100

    Projectpath tests (#567)
    * Adjust spacing
    
    to PEPLiam, because I'm now the most recent one to touch the file
    
    * Remove unnecessary lines
    
    * Remove unused import

--- tests/project/test_projectPath.py
+++ tests/project/test_projectPath.py
@@ -3,7 +3,6 @@
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
 import os
-import unittest
 from pyiron_base.project.path import ProjectPath
 from pyiron_base._tests import PyironTestCase
 from pyiron_base.state import state
@@ -13,9 +12,7 @@ class TestProjectPath(PyironTestCase):
     @classmethod
     def setUpClass(cls):
         if os.name == "nt":
-            cls.current_dir = os.path.dirname(os.path.abspath(__file__)).replace(
-                "\\", "/"
-            )
+            cls.current_dir = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
         else:
             cls.current_dir = os.path.dirname(os.path.abspath(__file__))
         cls.settings_root_paths = state.settings.configuration["project_paths"]
@@ -24,19 +21,15 @@ class TestProjectPath(PyironTestCase):
 
     def test_open(self):
         with self.project_path.open("test_open") as test_open:
-            self.assertEqual(
-                test_open.path, self.current_dir + "/test_project_path/test_open/"
-            )
+            self.assertEqual(test_open.path, self.current_dir + "/test_project_path/test_open/")
+
         self.project_path.removedirs("test_open")
 
     def test_close(self):
         with self.project_path.open("test_close") as test_close:
-            self.assertEqual(
-                test_close.path, self.current_dir + "/test_project_path/test_close/"
-            )
-        self.assertEqual(
-            self.project_path.path, self.current_dir + "/test_project_path/"
-        )
+            self.assertEqual(test_close.path, self.current_dir + "/test_project_path/test_close/")
+
+        self.assertEqual(self.project_path.path, self.current_dir + "/test_project_path/")
         self.project_path.removedirs("test_close")
 
     def test_copy(self):
@@ -58,14 +51,18 @@ class TestProjectPath(PyironTestCase):
 
     def test_root_path(self):
         root_paths = self.settings_root_paths
-        self.assertIn(self.project_path.root_path, root_paths, msg="root project.root_path not properly set by "
-                                                                   "default. Check if `project_check_enabled`.")
+        self.assertIn(
+            self.project_path.root_path, root_paths,
+            msg="root project.root_path not properly set by default. Check if `project_check_enabled`."
+        )
 
     def test_project_path(self):
         root_paths = self.settings_root_paths
-        self.assertIn(self.current_dir + '/test_project_path/',
-                      [root_path + self.project_path.project_path for root_path in root_paths],
-                      msg="project.project_path not properly set by default. Check if `project_check_enabled`.")
+        self.assertIn(
+            self.current_dir + '/test_project_path/',
+            [root_path + self.project_path.project_path for root_path in root_paths],
+            msg="project.project_path not properly set by default. Check if `project_check_enabled`."
+        )
 
     def test__get_project_from_path(self):
         old_state = state.settings.configuration["project_check_enabled"]
@@ -78,7 +75,3 @@ class TestProjectPath(PyironTestCase):
             self.assertEqual(pr_path, path)
         finally:
             state.settings.configuration["project_check_enabled"] = old_state
-
-
-if __name__ == "__main__":
-    unittest.main()
