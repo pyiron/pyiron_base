cf9a092fa60b63cbbf64d0288e935e54ea9c01bd
liamhuber <liam.huber@gmail.com>
2021-11-29 13:27:47 +0100

    Adjust spacing
    to PEPLiam, because I'm now the most recent one to touch the file

--- tests/project/test_projectPath.py
+++ tests/project/test_projectPath.py
@@ -13,9 +13,7 @@ class TestProjectPath(PyironTestCase):
     @classmethod
     def setUpClass(cls):
         if os.name == "nt":
-            cls.current_dir = os.path.dirname(os.path.abspath(__file__)).replace(
-                "\\", "/"
-            )
+            cls.current_dir = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
         else:
             cls.current_dir = os.path.dirname(os.path.abspath(__file__))
         cls.settings_root_paths = state.settings.configuration["project_paths"]
@@ -24,19 +22,15 @@ class TestProjectPath(PyironTestCase):
 
     def test_open(self):
         with self.project_path.open("test_open") as test_open:
-            self.assertEqual(
-                test_open.path, self.current_dir + "/test_project_path/test_open/"
-            )
+            self.assertEqual(test_open.path, self.current_dir + "/test_project_path/test_open/")
+
         self.project_path.removedirs("test_open")
 
     def test_close(self):
         with self.project_path.open("test_close") as test_close:
-            self.assertEqual(
-                test_close.path, self.current_dir + "/test_project_path/test_close/"
-            )
-        self.assertEqual(
-            self.project_path.path, self.current_dir + "/test_project_path/"
-        )
+            self.assertEqual(test_close.path, self.current_dir + "/test_project_path/test_close/")
+
+        self.assertEqual(self.project_path.path, self.current_dir + "/test_project_path/")
         self.project_path.removedirs("test_close")
 
     def test_copy(self):
@@ -58,14 +52,18 @@ class TestProjectPath(PyironTestCase):
 
     def test_root_path(self):
         root_paths = self.settings_root_paths
-        self.assertIn(self.project_path.root_path, root_paths, msg="root project.root_path not properly set by "
-                                                                   "default. Check if `project_check_enabled`.")
+        self.assertIn(
+            self.project_path.root_path, root_paths,
+            msg="root project.root_path not properly set by default. Check if `project_check_enabled`."
+        )
 
     def test_project_path(self):
         root_paths = self.settings_root_paths
-        self.assertIn(self.current_dir + '/test_project_path/',
-                      [root_path + self.project_path.project_path for root_path in root_paths],
-                      msg="project.project_path not properly set by default. Check if `project_check_enabled`.")
+        self.assertIn(
+            self.current_dir + '/test_project_path/',
+            [root_path + self.project_path.project_path for root_path in root_paths],
+            msg="project.project_path not properly set by default. Check if `project_check_enabled`."
+        )
 
     def test__get_project_from_path(self):
         old_state = state.settings.configuration["project_check_enabled"]
