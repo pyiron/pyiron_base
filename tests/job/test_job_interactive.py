ee26136507bb3099498260d643bd433570c06b52
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-12-06 14:31:49 -0700

    Create test_job_interactive.py

---
+++ tests/job/test_job_interactive.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+import unittest
+from pyiron_base.job.interactive import InteractiveBase
+from pyiron_base._tests import TestWithProject
+
+
+class TestJobInteractive(TestWithProject):
+    def test_job_with(self):
+        job = self.project.create_job(InteractiveBase, "test")
+        with self.assertRaises(NotImplementedError):
+            with job as _:
+                pass
+                
+    def test_job_interactive_with(self):
+        job = self.project.create_job(InteractiveBase, "test")
+        with job.interactive_open() as _:
+            pass
