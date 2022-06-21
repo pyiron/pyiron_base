f2b3e365f256108349c25fac4685e4227970d458
Marvin Poul <poul@mpie.de>
2022-06-21 12:22:02 +0200

    Properly check for None

--- tests/job/test_copyto.py
+++ tests/job/test_copyto.py
@@ -12,7 +12,7 @@ class TestCopyTo(TestWithProject):
         job_ser = self.project.create.job.SerialMasterBase("sequence_single")
         ham = self.project.create.job.ScriptJob("job_single")
         ham.copy_to(job_ser)
-        self.assertTrue(job_ser['job_single/input/custom_dict'])
+        self.assertTrue(job_ser['job_single/input/custom_dict'] is not None)
         job_ser.remove()
 
         job = self.project.create.job.ScriptJob("job_script")
