bc4bfa466fba2a76c36ce465d9b800e502232acc
Jan Janssen <jan-janssen@users.noreply.github.com>
2022-11-29 14:17:49 -0700

    Update test_parallelmaster.py

--- tests/master/test_parallelmaster.py
+++ tests/master/test_parallelmaster.py
@@ -30,6 +30,10 @@ class TestMaster(ParallelMaster):
         super().__init__(job_name, project)
         self._job_generator = TestGenerator(self)
 
+    # Implement since required
+    def collect_output(self):
+        pass
+
 
 class TestParallelMaster(TestWithProject):
 
