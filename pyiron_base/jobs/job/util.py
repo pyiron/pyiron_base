4e4f44d43a1a7c7fb8fddd4a13a8d323492699b6
Marvin Poul <poul@mpie.de>
2022-12-04 18:14:11 +0100

    More line separator replacements

--- pyiron_base/jobs/job/util.py
+++ pyiron_base/jobs/job/util.py
@@ -362,12 +362,12 @@ def _job_read_file(job, file_name, tail=None):
             with open(file_name) as f:
                 return f.readlines()
         else:
-            lines = list(reversed([l + "\n" for l in islice(monty.io.reverse_readfile(file_name), tail)]))
+            lines = list(reversed([l + os.linesep for l in islice(monty.io.reverse_readfile(file_name), tail)]))
             # compatibility with the other methods
             # monty strips all newlines, where as reading the other ways does
             # not.  So if a file does not end with a newline (as most text
             # files) adding it to every line like above adds an additional one.
-            lines[-1] = lines[-1].rstrip("\n")
+            lines[-1] = lines[-1].rstrip(os.linesep)
             return lines
 
 
