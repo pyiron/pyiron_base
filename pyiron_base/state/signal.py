f071aa0e0f86e4bea64daf7abb22434fb2058d48
Marvin Poul <poul@mpie.de>
2023-01-06 00:27:30 +0100

    Add new signal module

---
+++ pyiron_base/state/signal.py
@@ -0,0 +1,26 @@
+import contextlib
+import signal
+import sys
+
+intercepted_signals = [
+    signal.SIGINT,
+    signal.SIGTERM,
+    signal.SIGABRT,
+]  # , signal.SIGQUIT]
+
+@contextlib.contextmanager
+def catch_signals(cleanup):
+    def handler(sig, frame):
+        cleanup(sig)
+        if sig == signal.SIGINT:
+            raise KeyboardInterrupt()
+        elif sig in (signal.SIGTERM, signal.SIGABRT):
+            sys.exit(128 & sig)
+
+    for sig in intercepted_signals:
+        signal.signal(sig, handler)
+
+    try:
+        yield
+    finally:
+        signal.signal(sig, signal.SIG_DFL)
