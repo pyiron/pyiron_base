bb4a624ce49529ac9c349a6f8f90ed6c2812ebc8
Jan Janßen <janssen@mpie.de>
2023-01-31 21:46:56 -0700

    Use mapping

--- pyiron_base/database/generic.py
+++ pyiron_base/database/generic.py
@@ -1180,7 +1180,10 @@ class DatabaseAccess(IsDatabase):
         row = result.fetchall()
         if not self._keep_connection:
             self.conn.close()
-        return [dict(zip(col.keys(), col._mapping.values())) for col in row]
+        return [
+            dict(zip(col._mapping.keys(), col._mapping.values()))
+            for col in row
+        ]
 
     def get_job_status(self, job_id):
         try:
