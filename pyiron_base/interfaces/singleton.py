28b9b951b85f8ad29f9d5e67e2e032fe5c84c07d
liamhuber <liamhuber@greyhavensolutions.com>
2023-04-25 12:07:45 -0700

    Introduce a new updateable singleton

--- pyiron_base/interfaces/singleton.py
+++ pyiron_base/interfaces/singleton.py
@@ -34,3 +34,48 @@ class Singleton(ABCMeta):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
+class UpdatableSingleton(ABCMeta):
+    """
+    Like the `Singeton` class, but will re-run `__init__` on each instantiation.
+
+    This means that subsequent reinitializations have access to the existing state of
+    the object, against which comparisons can be made.
+
+    Examples)
+    >>> class Foo(metaclass=UpdatableSingleton):
+    ...      def __init__(self, x=None):
+    ...         try:
+    ...             if x is not None and x != self.x:
+    ...                 self.x = x
+    ...                 self.y = self.compute_y(x)
+    ...         except AttributeError:
+    ...             self.x = x
+    ...             self.y = self.compute_y(x)
+    ...
+    ...     def compute_y(self, x):
+    ...         # Imagine this was expensive and we wanted to avoid it
+    ...         return x**2 if x is not None else None
+    >>>
+    >>> foo = Foo(5)
+    >>> print(foo.x, foo.y)
+    ... 5 25
+    >>> bar = Foo(6)
+    >>> print(foo.x, foo.y, bar.x, bar.y)
+    ... 6 36 6 36
+    >>> # A singleton, but with updated values!
+    >>> baz = Foo()
+    >>> print(foo.x, foo.y, bar.x, bar.y)
+    ... 6 36
+    >>> # And we wrote Foo so it was easy to recover the object with its existing state
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(UpdatableSingleton, cls).__call__(*args, **kwargs)
+        else:
+            cls._instances[cls].__init__(*args, **kwargs)
+        return cls._instances[cls]
