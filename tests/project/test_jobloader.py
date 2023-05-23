2a2ab6d42943df6cfd5d0ab5b6705a6042a94a67
liamhuber <liamhuber@greyhavensolutions.com>
2023-05-23 13:01:47 -0700

    Remove sibling tests

--- tests/project/test_jobloader.py
+++ tests/project/test_jobloader.py
@@ -8,41 +8,41 @@ from pyiron_base.jobs.job.path import JobPath
 
 class TestLoaders(TestWithFilledProject):
 
-    def test_load(self):
-        loaded = self.project.load("toy_1")
-        self.assertIsInstance(
-            loaded, ToyJob, msg="Expected to load the full object"
-        )
+    # def test_load(self):
+    #     loaded = self.project.load("toy_1")
+    #     self.assertIsInstance(
+    #         loaded, ToyJob, msg="Expected to load the full object"
+    #     )
+    #
+    #     self.assertEqual(
+    #         loaded.name,
+    #         self.project.load.toy_1.name,
+    #         # TODO: we'd rather compare the jobs directly, but equality comparison
+    #         #       for jobs is not implemented
+    #         msg="Attribute access should function the same as explicit calls"
+    #     )
+    #
+    #     self.assertEqual(
+    #         len(self.project.job_table(recursive=False)),
+    #         len(self.project.load.__dir__()),
+    #         msg="Tab completion (`__dir__`) should see both jobs at this project "
+    #             "level"
+    #     )
 
-        self.assertEqual(
-            loaded.name,
-            self.project.load.toy_1.name,
-            # TODO: we'd rather compare the jobs directly, but equality comparison
-            #       for jobs is not implemented
-            msg="Attribute access should function the same as explicit calls"
-        )
-
-        self.assertEqual(
-            len(self.project.job_table(recursive=False)),
-            len(self.project.load.__dir__()),
-            msg="Tab completion (`__dir__`) should see both jobs at this project "
-                "level"
-        )
-
-    def test_inspect(self):
-        not_fully_loaded = self.project.load("toy_1", convert_to_object=False)
-        self.assertIsInstance(
-            not_fully_loaded,
-            JobPath,
-            msg="Expected to load only an HDF interface"
-        )
-
-        inspected = self.project.inspect("toy_1")
-        self.assertIsInstance(
-            inspected,
-            JobPath,
-            msg="Expected to load only an HDF interface"
-        )
+    # def test_inspect(self):
+    #     not_fully_loaded = self.project.load("toy_1", convert_to_object=False)
+    #     self.assertIsInstance(
+    #         not_fully_loaded,
+    #         JobPath,
+    #         msg="Expected to load only an HDF interface"
+    #     )
+    #
+    #     inspected = self.project.inspect("toy_1")
+    #     self.assertIsInstance(
+    #         inspected,
+    #         JobPath,
+    #         msg="Expected to load only an HDF interface"
+    #     )
 
     def test_without_database(self):
         settings_configuration = self.project.state.settings.configuration.copy()
