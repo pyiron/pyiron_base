5b48c9df4d43f16a4e6a0978631c98fb733f24b6
liamhuber <liamhuber@greyhavensolutions.com>
2023-05-23 15:51:28 -0700

    Revert changes, the problem is upstream in FileTable still
    I don't see trouble on my local machine because I don't specify TOP_LEVEL_DIRS in my config but the CI does.

--- tests/project/test_jobloader.py
+++ tests/project/test_jobloader.py
@@ -8,66 +8,46 @@ from pyiron_base.jobs.job.path import JobPath
 
 class TestLoaders(TestWithFilledProject):
 
-    # def test_load(self):
-    #     loaded = self.project.load("toy_1")
-    #     self.assertIsInstance(
-    #         loaded, ToyJob, msg="Expected to load the full object"
-    #     )
-    #
-    #     self.assertEqual(
-    #         loaded.name,
-    #         self.project.load.toy_1.name,
-    #         # TODO: we'd rather compare the jobs directly, but equality comparison
-    #         #       for jobs is not implemented
-    #         msg="Attribute access should function the same as explicit calls"
-    #     )
-    #
-    #     self.assertEqual(
-    #         len(self.project.job_table(recursive=False)),
-    #         len(self.project.load.__dir__()),
-    #         msg="Tab completion (`__dir__`) should see both jobs at this project "
-    #             "level"
-    #     )
+    def test_load(self):
+        loaded = self.project.load("toy_1")
+        self.assertIsInstance(
+            loaded, ToyJob, msg="Expected to load the full object"
+        )
 
-    # def test_inspect(self):
-    #     not_fully_loaded = self.project.load("toy_1", convert_to_object=False)
-    #     self.assertIsInstance(
-    #         not_fully_loaded,
-    #         JobPath,
-    #         msg="Expected to load only an HDF interface"
-    #     )
-    #
-    #     inspected = self.project.inspect("toy_1")
-    #     self.assertIsInstance(
-    #         inspected,
-    #         JobPath,
-    #         msg="Expected to load only an HDF interface"
-    #     )
+        self.assertEqual(
+            loaded.name,
+            self.project.load.toy_1.name,
+            # TODO: we'd rather compare the jobs directly, but equality comparison
+            #       for jobs is not implemented
+            msg="Attribute access should function the same as explicit calls"
+        )
+
+        self.assertEqual(
+            len(self.project.job_table(recursive=False)),
+            len(self.project.load.__dir__()),
+            msg="Tab completion (`__dir__`) should see both jobs at this project "
+                "level"
+        )
+
+    def test_inspect(self):
+        not_fully_loaded = self.project.load("toy_1", convert_to_object=False)
+        self.assertIsInstance(
+            not_fully_loaded,
+            JobPath,
+            msg="Expected to load only an HDF interface"
+        )
+
+        inspected = self.project.inspect("toy_1")
+        self.assertIsInstance(
+            inspected,
+            JobPath,
+            msg="Expected to load only an HDF interface"
+        )
 
     def test_without_database(self):
         settings_configuration = self.project.state.settings.configuration.copy()
 
-
-        import logging
-        logging.warning(f"JOB LOADER: type(self.project.db) = {type(self.project.db)}")
-        logging.warning(f"JOB LOADER: len(self.project.job_table()) = {len(self.project.job_table())}")
-        logging.warning(f"JOB LOADER: len(self.project.job_table(recursive=False)) = {len(self.project.job_table(recursive=False))}")
-
         self.project.state.update({'disable_database': True})
-        logging.warning(f"JOB LOADER: type(self.project.db) = {type(self.project.db)}")
-        logging.warning(f"JOB LOADER: self.project.project_path = {self.project.project_path}")
-
-        logging.warning(f"JOB LOADER: len(self.project.job_table()) = {len(self.project.job_table())}")
-        logging.warning(f"JOB LOADER: len(self.project.job_table(recursive=False)) = {len(self.project.job_table(recursive=False))}")
-
-        logging.warning(f"JOB LOADER: len(self.project.db.job_table(sql_query=self.project.sql_query, user=self.project.user, project_path=self.project.project_path)) = {len(self.project.db.job_table(sql_query=self.project.sql_query, user=self.project.user, project_path=self.project.project_path))}")
-        logging.warning(f"JOB LOADER: len(self.project.db.job_table(sql_query=self.project.sql_query, user=self.project.user, project_path=self.project.project_path, recursive=False)) = {len(self.project.db.job_table(sql_query=self.project.sql_query, user=self.project.user, project_path=self.project.project_path, recursive=False))}")
-
-        logging.warning(f"JOB LOADER: len(self.project.db._get_job_table(sql_query=self.project.sql_query, user=self.project.user, project_path=self.project.project_path)) = {len(self.project.db._get_job_table(sql_query=self.project.sql_query, user=self.project.user, project_path=self.project.project_path))}")
-        logging.warning(f"JOB LOADER: len(self.project.db._get_job_table(sql_query=self.project.sql_query, user=self.project.user, project_path=self.project.project_path, recursive=False)) = {len(self.project.db._get_job_table(sql_query=self.project.sql_query, user=self.project.user, project_path=self.project.project_path, recursive=False))}")
-
-        logging.warning(f"JOB LOADER: len(self.project.db._get_job_table(sql_query=self.project.sql_query, user=self.project.user, project_path=None, recursive=False)) = {len(self.project.db._get_job_table(sql_query=self.project.sql_query, user=self.project.user, project_path=None, recursive=False))}")
-        logging.warning(f"JOB LOADER: len(self.project.db._get_job_table(sql_query=self.project.sql_query, user=self.project.user, project_path=None, recursive=False)) = {len(self.project.db._get_job_table(sql_query=self.project.sql_query, user=self.project.user, project_path=None, recursive=False))}")
 
         with self.subTest("Works without the database"):
             self.assertIsInstance(
