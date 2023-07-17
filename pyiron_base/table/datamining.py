ead9c4de7059b84032b7c20ad5ce3d838f94daf8
pyiron-runner <pyiron@mpie.de>
2023-07-17 13:49:53 +0000

    Format black

--- pyiron_base/table/datamining.py
+++ pyiron_base/table/datamining.py
@@ -1,6 +1,8 @@
 from pyiron_base.jobs.datamining import *
 import warnings
+
 warnings.warning(
     "pyiron_base.table.datamining is deprecated, import from pyiron_base.job.datamining",
-    category=DeprecationWarning, stacklevel=1
+    category=DeprecationWarning,
+    stacklevel=1,
 )
