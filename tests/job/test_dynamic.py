1cf5786ed9001aedf221bc6a78dce1e81f855c84
Jan Janßen <janssen@mpie.de>
2023-12-18 13:59:48 +0100

    update test

--- tests/job/test_dynamic.py
+++ tests/job/test_dynamic.py
@@ -1,8 +1,8 @@
-import unittest
 import json
 import os
 from pyiron_base import Project
 from pyiron_base.jobs.dynamic import create_new_job_type
+from pyiron_base._tests import TestWithProject
 
 
 def write_input(input_dict, working_directory="."):
@@ -15,18 +15,20 @@ def collect_output(working_directory="."):
         return json.load(f)
 
 
-class DynamicTest(unittest.TestCase):
+class DynamicTest(TestWithProject):
     def test_dynamic_job_from_functions(self):
+        class_name = "MyDynamicJob"
         job_type = create_new_job_type(
-            class_name="MyDynamicJob",
+            class_name=class_name,
             input_dict={"energy": 1.0},
             write_input_funct=write_input,
             collect_output_funct=collect_output,
             executable_str="cat input.json > output.json"
         )
-        pr = Project(".")
-        job = pr.create_job(job_type=job_type, job_name="test")
+        job = self.project.create_job(job_type=job_type, job_name="test")
         job.input.energy = 2.0
         job.run()
+        self.assertEqual(job["NAME"], class_name)
+        self.assertEqual(job["TYPE"], "<class 'abc." + class_name + "'>")
         self.assertEqual(job.output.energy, 2.0)
-        pr.remove_jobs(recursive=True, silently=True)
+        self.project.remove_jobs(recursive=True, silently=True)
