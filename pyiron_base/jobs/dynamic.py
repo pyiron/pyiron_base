dc064a35a16dea450f2fc18d630769606714f4bd
Jan Janssen <janssen@mpie.de>
2024-01-16 21:21:49 +0100

    fixes

--- pyiron_base/jobs/dynamic.py
+++ pyiron_base/jobs/dynamic.py
@@ -4,15 +4,12 @@ import os
 def warn_dynamic_job_classes(resource_folder_lst, logger):
     for path in resource_folder_lst:
         sub_folders = os.listdir(path)
-        if "dynamic" in sub_folders:
-            logger.warning(
-                "pyiron found a 'dynamic' folder in the "
-                + path
-                + " resource directory. These are no longer supported in pyiron_base >=0.7.0."
-            )
-        elif "templates" in sub_folders:
-            logger.warning(
-                "pyiron found a 'templates' folder in the "
-                + path
-                + " resource directory. These are no longer supported in pyiron_base >=0.7.0."
-            )
+        for folder in ["dynamic", "templates"]:
+            if folder in sub_folders:
+                logger.warning(
+                    "pyiron found a '"
+                    + folder
+                    + "' folder in the "
+                    + path
+                    + " resource directory. These are no longer supported in pyiron_base >=0.7.0."
+                )
