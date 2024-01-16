4f92850d6087a260e4083f1adab0a3ddf24a6427
Jan Janssen <janssen@mpie.de>
2024-01-16 21:33:08 +0100

    Check if path exists

--- pyiron_base/jobs/dynamic.py
+++ pyiron_base/jobs/dynamic.py
@@ -3,13 +3,14 @@ import os
 
 def warn_dynamic_job_classes(resource_folder_lst, logger):
     for path in resource_folder_lst:
-        sub_folders = os.listdir(path)
-        for folder in ["dynamic", "templates"]:
-            if folder in sub_folders:
-                logger.warning(
-                    "pyiron found a '"
-                    + folder
-                    + "' folder in the "
-                    + path
-                    + " resource directory. These are no longer supported in pyiron_base >=0.7.0."
-                )
+        if os.path.exists(path):
+            sub_folders = os.listdir(path)
+            for folder in ["dynamic", "templates"]:
+                if folder in sub_folders:
+                    logger.warning(
+                        "pyiron found a '"
+                        + folder
+                        + "' folder in the "
+                        + path
+                        + " resource directory. These are no longer supported in pyiron_base >=0.7.0."
+                    )
