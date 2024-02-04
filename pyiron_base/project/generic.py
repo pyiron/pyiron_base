84fce1a451048e6aeba1ee882326ad4bebad42e6
Jan Janßen <janssen@mpie.de>
2024-02-04 15:43:26 +0100

    Define size conversion as internal function

--- pyiron_base/project/generic.py
+++ pyiron_base/project/generic.py
@@ -213,6 +213,26 @@ class Project(ProjectPath, HasGroups):
         Get the size of the project
         """
         import pint
+
+        def _size_conversion(size: pint.Quantity):
+            sign_prefactor = 1
+            if size < 0:
+                sign_prefactor = -1
+                size *= -1
+            elif size == 0:
+                return size
+
+            prefix_index = math.floor(math.log2(size) / 10) - 1
+            prefix = ["Ki", "Mi", "Gi", "Ti", "Pi"]
+
+            size *= sign_prefactor
+            if prefix_index < 0:
+                return size
+            elif prefix_index < 5:
+                return size.to(f"{prefix[prefix_index]}byte")
+            else:
+                return size.to(f"{prefix[-1]}byte")
+
         size = (
             sum(
                 [
@@ -222,27 +242,7 @@ class Project(ProjectPath, HasGroups):
             )
             * pint.UnitRegistry().byte
         )
-        return self._size_conversion(size)
-
-    @staticmethod
-    def _size_conversion(size):
-        sign_prefactor = 1
-        if size < 0:
-            sign_prefactor = -1
-            size *= -1
-        elif size == 0:
-            return size
-
-        prefix_index = math.floor(math.log2(size) / 10) - 1
-        prefix = ["Ki", "Mi", "Gi", "Ti", "Pi"]
-
-        size *= sign_prefactor
-        if prefix_index < 0:
-            return size
-        elif prefix_index < 5:
-            return size.to(f"{prefix[prefix_index]}byte")
-        else:
-            return size.to(f"{prefix[-1]}byte")
+        return _size_conversion(size)
 
     def copy(self):
         """
