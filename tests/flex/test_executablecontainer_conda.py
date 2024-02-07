b33fa48425315b713b2eaa1078a94b30834140e1
Jan Janssen <janssen@mpie.de>
2024-02-07 10:28:50 +0100

    Add conda as dependency

--- tests/flex/test_executablecontainer_conda.py
+++ tests/flex/test_executablecontainer_conda.py
@@ -1,5 +1,13 @@
 import os
+import unittest
 from pyiron_base._tests import TestWithProject
+
+
+try:
+    import conda
+    skip_conda_test = False
+except ImportError:
+    skip_conda_test = True
 
 
 conda_env = """\
@@ -10,23 +18,36 @@ dependencies:
 """
 
 
+@unittest.skipIf(skip_conda_test, "conda is not available, so the conda environment related tests are skipped.")
 class TestExecutableContainerConda(TestWithProject):
-    def setUp(self):
-        super().setUp()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         with open("env.yaml", "w") as f:
             f.writelines(conda_env)
+        cls.project.conda_environment.create(env_name="py312", env_file="env.yaml")
 
-    def tearDown(self):
-        super().tearDown()
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
         os.remove("env.yaml")
 
-    def test_create_job_class(self):
+    def test_conda_environment_path(self):
         self.project.create_job_class(
             class_name="PythonVersionJob",
             executable_str="python --version",
         )
-        self.project.conda_environment.create(env_name="py312", env_file="env.yaml")
-        job = self.project.create.job.PythonVersionJob(job_name="job_conda")
+        job = self.project.create.job.PythonVersionJob(job_name="job_conda_path")
         job.server.conda_environment_path = self.project.conda_environment.py312
+        job.run()
+        self.assertEqual(job["error.out"][0], "Python 3.12.1\n")
+
+    def test_conda_environment_name(self):
+        self.project.create_job_class(
+            class_name="PythonVersionJob",
+            executable_str="python --version",
+        )
+        job = self.project.create.job.PythonVersionJob(job_name="job_conda_name")
+        job.server.conda_environment_name = "py312"
         job.run()
         self.assertEqual(job["error.out"][0], "Python 3.12.1\n")
