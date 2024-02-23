29dca99cff4c5a4a9ff8325019cfabc59b4acd20
liamhuber <liamhuber@greyhavensolutions.com>
2024-02-23 12:54:09 -0800

    Clarify intent to future devs

--- pyiron_base/jobs/flex/pythonfunctioncontainer.py
+++ pyiron_base/jobs/flex/pythonfunctioncontainer.py
@@ -44,8 +44,12 @@ class PythonFunctionContainerJob(PythonTemplateJob):
         super().__init__(project, job_name)
         self._function = None
         self._executor_type = None
-        self._automatically_rename_on_save = False  # Automatically rename job using function and
-        # input values
+        self._automatically_rename_on_save = False
+        # Automatically rename job using function and input values at save time
+        # This is useful for the edge case where these jobs are created from a wrapper
+        # and automatically assigned a name based on the function name, but multiple
+        # jobs are created from the same function (and thus distinguished only by their
+        # input)
 
     @property
     def python_function(self):
