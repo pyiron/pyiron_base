f9b9f3a97c3b144b9f2beab9f1cbf7eb86465200
Marvin Poul <poul@mpie.de>
2024-03-02 08:50:09 +0100

    Add two classes for HasHDF/HasDict compat
    and outline transition path in docstrings there.

--- pyiron_base/interfaces/has_dict.py
+++ pyiron_base/interfaces/has_dict.py
@@ -1,9 +1,25 @@
 # coding: utf-8
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
-"""Interface for classes to serialize to dictionary."""
+"""Interface for classes to serialize to dictionary.
+
+This also contains classes to ease the transition from HDF based storage to
+dict based serialization.  Roughly we want to proceed as follows:
+    1. Any new object directly implements :class:`.HasDict` and derives from
+    :class:`.HasHDFfromDict` to be compatible to older code that still relies
+    on the HDF interface.
+    2. Any old object that doesn't yet directly implements :class:`.HasDict`
+    can trivially derive from :class:`.HasDictfromHDF` to be compatible to
+    newer code that uses the dict based serialization.
+    3. Step by step we can transition old objects to directly implement
+    :class:`.HasDict`.
+"""
 
 from abc import ABC, abstractmethod
+from typing import Any
+
+from pyiron_base.interfaces.has_hdf import HasHDF
+from pyiron_base.storage.hdfio import DummyHDFio
 
 __author__ = "Jan Janssen"
 __copyright__ = (
@@ -25,10 +41,10 @@ class HasDict(ABC):
         pass
 
     @abstractmethod
-    def to_dict(self):
+    def _to_dict(self):
         pass
 
-    def _type_to_dict(self):
+    def to_dict(self):
         type_dict = {
             "NAME": self.__class__.__name__,
             "TYPE": str(type(self)),
@@ -37,4 +53,60 @@ class HasDict(ABC):
         }
         if hasattr(self, "__version__"):
             type_dict["VERSION"] = self.__version__
-        return type_dict
+        return self._to_dict() | type_dict
+
+    @staticmethod
+    def _join_children_dict(
+            children: dict[str, dict[str, Any]]
+    ) -> dict[str, Any]:
+        """
+        Given a nested dictionary, flatten the first level.
+
+        >>> d = {'a': {'a1': 3}, 'b': {'b1': 4, 'b2': {'c': 42}}}
+        >>> _join_children_dict(d)
+        {'a/a1': 3, 'b/b1': 4, 'b/b2': {'c': 42}}
+
+        This is intended as a utility function for nested HasDict objects, that
+        to_dict their children and then want to give a flattened dict for
+        writing to ProjectHDFio.write_dict_to_hdf
+        """
+        return {'/'.join((k1, k2)): v2 for k1, v1 in children.items()
+                                        for k2, v2 in v2.items()}
+
+
+class HasHDFfromDict(HasHDF, HasDict):
+    """
+    Implements HasHDF in terms of HasDict.
+
+    This class is intended for "new-style" objects that are used in a context
+    that only assumes that they implements HasHDF.
+
+    Implementors may still override :meth:`.HasHDF._get_hdf_group_name`.
+    """
+
+    def _from_hdf(self, hdf, version=None):
+        self.from_dict(hdf.read_dict_from_hdf(recursive=True))
+
+    def _to_hdf(self, hdf):
+        hdf.write_dict_to_hdf(self.to_dict())
+
+class HasDictfromHDF(HasDict, HasHDF):
+    """
+    Implements HasDict in terms of HasHDF.
+
+    This class is intended for "old-style" objects that should be able to be
+    used as children for objects that already implement HasDict and expect
+    their children to implmement it.
+    """
+
+    def from_dict(self, obj_dict: dict, version: str = None):
+        # DummyHDFio(project=None) looks a bit weird, but it was added there
+        # only to support saving/loading jobs which already use the HasDict
+        # interface
+        hdf = DummyHDFio(None, '/', obj_dict)
+        self.from_hdf(hdf)
+
+    def _to_dict(self):
+        hdf = DummyHDFio(None, '/')
+        self.to_hdf(hdf)
+        return hdf.to_dict()
