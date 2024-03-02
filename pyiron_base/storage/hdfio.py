98ca5b6f6c519d44ac23d4fadedffa22e955bf4f
Marvin Poul <poul@mpie.de>
2024-03-02 08:52:46 +0100

    Add explicit error in DummyHDFio when project is not set

--- pyiron_base/storage/hdfio.py
+++ pyiron_base/storage/hdfio.py
@@ -1385,7 +1385,10 @@ class DummyHDFio(HasGroups):
 
     @property
     def project(self):
-        return self._project
+        if self._project is not None:
+            return self._project
+        else:
+            raise RuntimeError("No project set!")
 
     @property
     def h5_path(self):
