c87c28d5f502a4e4f59994a29e9815f3071e592f
Jan Janssen <janssen@mpie.de>
2024-03-11 13:43:44 -0500

    Add additional test

--- tests/flex/test_wrap_executable.py
+++ tests/flex/test_wrap_executable.py
@@ -2,8 +2,18 @@ import os
 from pyiron_base._tests import TestWithProject
 
 
-class TestExecutableContainer(TestWithProject):
-    def test_conda_environment_path(self):
+def write_input(input_dict, working_directory="."):
+    with open(os.path.join(working_directory, "input_file"), "w") as f:
+        f.write(str(input_dict["energy"]))
+
+
+def collect_output(working_directory="."):
+    with open(os.path.join(working_directory, "output_file"), "r") as f:
+        return {"energy": float(f.readline())}
+
+
+class TestWrapExecutable(TestWithProject):
+    def test_python_version(self):
         python_version_step = self.project.wrap_executable(
             job_name="pythonjobstep",
             executable_str="python --version",
@@ -20,3 +30,16 @@ class TestExecutableContainer(TestWithProject):
             python_version_step.files.error_out,
             os.path.join(python_version_step.working_directory, "error.out")
         )
+
+    def test_cat(self):
+        job = self.project.wrap_executable(
+            job_name="Cat_Job_energy_1_0",
+            write_input_funct=write_input,
+            collect_output_funct=collect_output,
+            input_dict={"energy": 1.0},
+            executable_str="cat input_file > output_file",
+            execute_job=False,
+        )
+        job.input.energy = 2.0
+        job.run()
+        self.assertEqual(job.output.energy, 2.0)
