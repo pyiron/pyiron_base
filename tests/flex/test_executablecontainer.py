b1c7312b7ebc8d1bfe5a3e11f6441bb0420b9f24
Jan Janssen <janssen@mpie.de>
2024-03-25 12:52:10 -0500

    remove debugging from test

--- tests/flex/test_executablecontainer.py
+++ tests/flex/test_executablecontainer.py
@@ -102,7 +102,7 @@ class TestExecutableContainer(TestWithProject):
         if os.name != "nt":
             self.assertEqual(job.files.error_out, output_file_path)
         else:
-            self.assertEqual(str(job.files.error_out), output_file_path.replace("\\", "/"))
+            self.assertEqual(job.files.error_out, output_file_path.replace("\\", "/"))
 
     def test_create_job_factory_typeerror(self):
         create_catjob = create_job_factory(
