b875398bb0ad3d0bf71fa04e1c1c47fe4fcaede5
Jan Janssen <janssen@mpie.de>
2024-04-27 18:17:46 -0600

    Modify GenericError to use the working_directory rather than the job

--- pyiron_base/jobs/job/generic.py
+++ pyiron_base/jobs/job/generic.py
@@ -166,7 +166,7 @@ class GenericJob(JobCore, HasDict):
         self._python_only_job = False
         self._write_work_dir_warnings = True
         self.interactive_cache = None
-        self.error = GenericError(job=self)
+        self.error = GenericError(working_directory=self.project_hdf5.working_directory)
 
     @property
     def version(self):
@@ -1566,8 +1566,8 @@ class GenericJob(JobCore, HasDict):
 
 
 class GenericError(object):
-    def __init__(self, job):
-        self._job = job
+    def __init__(self, working_directory):
+        self._working_directory = working_directory
 
     def __repr__(self):
         all_messages = ""
@@ -1585,7 +1585,7 @@ class GenericError(object):
         return self._print_error(file_name="error.out", string=string)
 
     def _print_error(self, file_name, string="", print_yes=True):
-        if self._job[file_name] is None:
+        if os.path.join(self._working_directory, file_name) is None:
             return ""
         elif print_yes:
-            return string.join(self._job[file_name])
+            return string.join(os.path.join(self._working_directory, file_name))
