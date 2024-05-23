32dd534bf2d764265b6f541c14f3536d83b33825
samwaseda <o.waseda@mpie.de>
2024-05-23 20:59:41 +0000

    remove the for loops

--- tests/archiving/test_import.py
+++ tests/archiving/test_import.py
@@ -29,15 +29,11 @@ class TestUnpacking(PyironTestCase):
     @classmethod
     def tearDownClass(cls) -> None:
         super().tearDownClass()
-        # for job_id in cls.pr.job_table().id:
-        #     cls.pr.remove_job(job_id, _unprotect=True)
         cls.pr.remove(enable=True)
         uncompressed_pr = Project(cls.arch_dir)
         uncompressed_pr.remove(enable=True, enforce=True)
         os.remove("export.csv")
         os.remove(cls.arch_dir_comp + ".tar.gz")
-        # for job_id in cls.imp_pr.job_table().id:
-        #     cls.imp_pr.remove_job(job_id, _unprotect=True)
         cls.imp_pr.remove(enable=True)
 
     def setUp(self):
@@ -47,8 +43,6 @@ class TestUnpacking(PyironTestCase):
 
     def tearDown(self):
         super().tearDown()
-        # for job_id in self.imp_pr.job_table().id:
-        #     self.imp_pr.remove_job(job_id, _unprotect=True)
         self.imp_pr.remove_jobs(recursive=True, silently=True)
 
     def test_import_csv(self):
