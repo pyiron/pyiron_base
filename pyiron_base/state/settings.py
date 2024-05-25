06fb9b55562931434bbed0b090c6f32ab2171be5
samwaseda <o.waseda@mpie.de>
2024-05-25 07:43:37 +0000

    move strtobool to utils

--- pyiron_base/state/settings.py
+++ pyiron_base/state/settings.py
@@ -38,6 +38,7 @@ import warnings
 from configparser import ConfigParser
 from pyiron_base.state.logger import logger
 from pyiron_base.state.publications import publications
+from pyiron_base.utils.strtobool import strtobool
 from pathlib import Path
 from pyiron_base.interfaces.singleton import Singleton
 from typing import Union, Dict, List
@@ -56,25 +57,6 @@ __date__ = "Sep 1, 2017"
 
 
 PYIRON_DICT_NAME = "PYIRON"
-
-
-def strtobool (val):
-    """Convert a string representation of truth to true (1) or false (0).
-
-    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
-    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
-    'val' is anything else.
-
-    Originally part of `distutils.util.strtobool`, but copied and pasted as
-    `distutils` was going to be removed from python 3.12.
-    """
-    val = val.lower()
-    if val in ('y', 'yes', 't', 'true', 'on', '1'):
-        return 1
-    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
-        return 0
-    else:
-        raise ValueError("invalid truth value %r" % (val,))
 
 
 class Settings(metaclass=Singleton):
