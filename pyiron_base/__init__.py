1dc1cd727513ddc5707fb8c1f9d5dcb91ef5e258
liamhuber <liamhuber@greyhavensolutions.com>
2024-05-31 10:21:15 -0700

    [patch] Use ImportAlarm from pyiron_snippets

--- pyiron_base/__init__.py
+++ pyiron_base/__init__.py
@@ -13,7 +13,6 @@ from pyiron_base.storage.has_stored_traits import HasStoredTraits
 from pyiron_base.storage.inputlist import InputList
 from pyiron_base.storage.parameters import GenericParameters
 from pyiron_base.utils.deprecate import Deprecator, deprecate, deprecate_soon
-from pyiron_base.utils.error import ImportAlarm
 from pyiron_base.jobs.job.extension.executable import Executable
 from pyiron_base.project.external import Notebook, load, dump
 from pyiron_base.jobs.dynamic import warn_dynamic_job_classes
@@ -77,7 +76,6 @@ __all__ = [
     Deprecator,
     deprecate,
     deprecate_soon,
-    ImportAlarm,
     Executable,
     Notebook,
     load,
--- tests/generic/test_util.py
+++ tests/generic/test_util.py
@@ -6,7 +6,6 @@ import unittest
 import warnings
 from pyiron_base.utils.instance import static_isinstance
 from pyiron_base.utils.deprecate import deprecate, deprecate_soon
-from pyiron_base.utils.error import ImportAlarm
 from pyiron_base._tests import PyironTestCase
 
 
@@ -143,72 +142,6 @@ class TestDeprecator(PyironTestCase):
             foo(bar=True)
             food(baz=True)
         self.assertEqual(len(w), 2, "Not all warnings preserved.")
-
-
-class TestImportAlarm(PyironTestCase):
-    def setUp(self):
-        super().setUp()
-        self.import_alarm = ImportAlarm()
-
-        @self.import_alarm
-        def add_one(x):
-            return x + 1
-
-        with ImportAlarm("Broken import") as alarm_broken:
-            import ASDF
-
-        @alarm_broken
-        def add_two(x):
-            return x + 2
-
-        with ImportAlarm("Working import") as alarm_working:
-            import sys
-
-        @alarm_working
-        def add_three(x):
-            return x + 3
-
-        self.add_one = add_one
-        self.add_two = add_two
-        self.add_three = add_three
-
-    def test_no_warning(self):
-        with warnings.catch_warnings(record=True) as w:
-            self.add_one(0)
-        self.assertEqual(
-            len(w), 0, "Expected no warnings, but got {} warnings.".format(len(w))
-        )
-
-    def test_has_warning(self):
-        self.import_alarm.message = "Now add_one should throw an ImportWarning"
-
-        with warnings.catch_warnings(record=True) as w:
-            self.add_one(1)
-        self.assertEqual(
-            len(w), 1, "Expected one warning, but got {} warnings.".format(len(w))
-        )
-
-    def test_context(self):
-        """
-        Usage via context manager should give same results and not suppress other errors.
-        """
-
-        with warnings.catch_warnings(record=True) as w:
-            self.add_two(0)
-        self.assertEqual(
-            len(w), 1, "Expected one warning, but got {} warnings.".format(len(w))
-        )
-
-        with warnings.catch_warnings(record=True) as w:
-            self.add_three(0)
-        self.assertEqual(
-            len(w), 0, "Expected one warning, but got {} warnings.".format(len(w))
-        )
-
-        with self.assertRaises(
-            ZeroDivisionError, msg="Context manager should swallow unrelated exceptions"
-        ), ImportAlarm("Unrelated"):
-            print(1 / 0)
 
 
 if __name__ == "__main__":
