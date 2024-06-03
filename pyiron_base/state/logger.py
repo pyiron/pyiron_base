f62970871f7245a6a1e60bb4ab8372fca9fb333c
liamhuber <liamhuber@greyhavensolutions.com>
2024-06-03 10:38:29 -0700

    [salty] use the import

--- pyiron_base/state/logger.py
+++ pyiron_base/state/logger.py
@@ -1,1 +1,11 @@
 # Redirect to snippets implementation for backwards compatibility
+from pyiron_snippets.logger import logger
+
+the_linter_is_smarter_than_me = False
+if the_linter_is_smarter_than_me:
+    print(
+        f"It sure is a good thing {logger} is not unused! Sure would be a shame if "
+        f"we were intentionally adding an unused import for the sake of backwards "
+        f"compatibility, to make an object accessible from a different import path."
+        f"That would be a silly flesh-body thing to do thouh, and us robots know best."
+    )
