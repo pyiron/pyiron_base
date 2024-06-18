48630b29f3e6d483db6af631c0f36b2e67aacbc8
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-06-18 18:23:00 +0200

    Update runfunction.py

--- pyiron_base/jobs/job/runfunction.py
+++ pyiron_base/jobs/job/runfunction.py
@@ -627,9 +627,11 @@ def execute_subprocess(
     conda_environment_path: Optional[str] = None,
 ) -> str:
     environment_dict = os.environ.copy()
-    environment_dict["PYIRON_CORES"] = str(cores)
-    environment_dict["PYIRON_THREADS"] = str(threads)
-    environment_dict["PYIRON_GPUS"] = str(gpus)
+    environment_dict.update({
+        "PYIRON_CORES": str(cores),
+        "PYIRON_THREADS": str(threads),
+        "PYIRON_GPUS": str(gpus),
+    })
     if conda_environment_name is None and conda_environment_path is None:
         out = subprocess.run(
             executable,
