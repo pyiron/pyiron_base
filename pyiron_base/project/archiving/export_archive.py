6fa7b10eb9445a56a0a4860030eb8eded8655623
samwaseda <o.waseda@mpie.de>
2024-07-24 05:59:29 +0000

    add docstring

--- pyiron_base/project/archiving/export_archive.py
+++ pyiron_base/project/archiving/export_archive.py
@@ -10,15 +10,37 @@ from pyiron_base.utils.instance import static_isinstance
 
 
 def new_job_id(job_id, job_translate_dict):
+    """
+    Translate a job ID using a provided dictionary.
+
+    Args:
+        job_id (float or int): The job ID to be translated. If it is a float, it will be converted to an integer.
+        job_translate_dict (dict): Dictionary mapping original job IDs to new job IDs.
+
+    Returns:
+        int or None: The translated job ID if it exists in the dictionary, otherwise None.
+    """
     if isinstance(job_id, float) and not np.isnan(job_id):
         job_id = int(job_id)
     if isinstance(job_id, int):
-        return job_translate_dict[job_id]
+        return job_translate_dict.get(job_id)
     else:
         return None
 
 
 def update_project(project_instance, directory_to_transfer, archive_directory, df):
+    """
+    Update the project paths in a DataFrame to reflect the new archive location.
+
+    Args:
+        project_instance (Project): The project instance for accessing project properties.
+        directory_to_transfer (str): The directory containing the jobs to be transferred.
+        archive_directory (str): The base directory for the archive.
+        df (DataFrame): DataFrame containing job information, including project paths.
+
+    Returns:
+        list: List of updated project paths reflecting the new archive location.
+    """
     directory_to_transfer = os.path.basename(directory_to_transfer)
     pr_transfer = project_instance.open(os.curdir)
     dir_name_transfer = getdir(path=directory_to_transfer)
@@ -37,6 +59,15 @@ def update_project(project_instance, directory_to_transfer, archive_directory, d
 
 
 def compress_dir(archive_directory):
+    """
+    Compress a directory into a tar.gz archive and remove the original directory.
+
+    Args:
+        archive_directory (str): The directory to be compressed.
+
+    Returns:
+        str: The name of the compressed tar.gz archive.
+    """
     arch_comp_name = archive_directory + ".tar.gz"
     with tarfile.open(arch_comp_name, "w:gz") as tar:
         tar.add(os.path.relpath(archive_directory, os.getcwd()))
@@ -48,17 +79,18 @@ def copy_files_to_archive(
     directory_to_transfer, archive_directory, compressed=True, copy_all_files=False
 ):
     """
-    Create an archive of jobs in directory_to_transfer.
+    Copy files from a directory to an archive, optionally compressing the archive.
 
     Args:
-        directory_to_transfer (str): project directory with jobs to export
-        archive_directory (str): name of the final archive; if no file ending is given .tar.gz is added automatically when needed
-        compressed (bool): if True compress archive_directory as a tarball; default True
-        copy_all_files (bool): if True include job output files in archive, otherwise just include .h5 files; default False
-    """
+        directory_to_transfer (str): The directory containing the files to transfer.
+        archive_directory (str): The destination directory for the archive.
+        compressed (bool): If True, compress the archive directory into a tarball. Default is True.
+        copy_all_files (bool): If True, include all files in the transfer, otherwise only .h5 files. Default is False.
 
+    Returns:
+        None
+    """
     assert isinstance(archive_directory, str) and ".tar.gz" not in archive_directory
-    # print("directory to transfer: "+directory_to_transfer)
     dst = os.path.join(archive_directory, getdir(path=directory_to_transfer))
     if copy_all_files:
         copytree(directory_to_transfer, dst, dirs_exist_ok=True)
@@ -71,12 +103,31 @@ def copy_files_to_archive(
 
 
 def ignore_non_h5_files(dir, files):
+    """
+    Ignore files that do not have a .h5 extension.
+
+    Args:
+        dir (str): The directory containing the files.
+        files (list): List of file names in the directory.
+
+    Returns:
+        list: List of file names that do not have a .h5 extension.
+    """
     return [f for f in files if not f.endswith(".h5")]
 
 
 def export_database(pr, directory_to_transfer, archive_directory):
-    # here we first check wether the archive directory is a path
-    # or a project object
+    """
+    Export the project database to an archive directory.
+
+    Args:
+        pr (Project): The project instance containing the jobs.
+        directory_to_transfer (str): The directory containing the jobs to transfer.
+        archive_directory (str): The destination directory for the archive.
+
+    Returns:
+        DataFrame: DataFrame containing updated job information with new IDs and project paths.
+    """
     assert isinstance(archive_directory, str) and ".tar.gz" not in archive_directory
     directory_to_transfer = os.path.basename(directory_to_transfer)
     df = pr.job_table()
@@ -103,3 +154,4 @@ def export_database(pr, directory_to_transfer, archive_directory):
     )
     del df["projectpath"]
     return df
+
