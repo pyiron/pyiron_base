c091dab262662d057e2f663764aa6831f9549369
samwaseda <o.waseda@mpie.de>
2024-08-02 15:46:31 +0000

    change path

--- pyiron_base/project/archiving/export_archive.py
+++ pyiron_base/project/archiving/export_archive.py
@@ -67,16 +67,12 @@ def copy_files_to_archive(
             # Compress the temporary directory into a tar.gz archive
             arch_comp_name = f"{archive_directory}.tar.gz"
             with tarfile.open(arch_comp_name, "w:gz") as tar:
-                tar.add(
-                    dst,
-                    arcname=os.path.join(
-                        getdir(archive_directory), dir_name_transfer
-                    )
-                )
+                tar.add(temp_dir, arcname=os.path.basename(archive_directory))
         else:
             # If not compressing, copy the directory to the final destination
             final_dst = os.path.join(archive_directory, dir_name_transfer)
             shutil.copytree(dst, final_dst, dirs_exist_ok=True)
+
 
 
 def copy_h5_files(src, dst):
