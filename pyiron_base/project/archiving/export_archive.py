7ca931eebe23fd8ac6a216e7246d7f741ec93892
samwaseda <o.waseda@mpie.de>
2024-08-04 20:38:43 +0000

    update tests

--- pyiron_base/project/archiving/export_archive.py
+++ pyiron_base/project/archiving/export_archive.py
@@ -74,7 +74,12 @@ def copy_files_to_archive(
         )
     elif compress and copy_all_files:
         with tarfile.open(f"{archive_directory}.tar.gz", "w:gz") as tar:
-            tar.add(directory_to_transfer, arcname=dir_name_transfer)
+            tar.add(
+                os.path.dirname(directory_to_transfer),
+                arcname=os.path.relpath(
+                    os.path.abspath(archive_directory), os.getcwd()
+                ),
+            )
     else:
         with tempfile.TemporaryDirectory() as temp_dir:
             # Copy files to the temporary directory
