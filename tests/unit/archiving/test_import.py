211e1ad474d0bb00a40adbe2f03f8735be0122bd
samwaseda <o.waseda@mpie.de>
2024-08-13 14:34:06 +0000

    remove redundant extension change

--- tests/unit/archiving/test_import.py
+++ tests/unit/archiving/test_import.py
@@ -174,7 +174,7 @@ class TestUnpacking(PyironTestCase):
             origin_path=pack_path_comp, csv_file_name=pack_path_csv, compress=True
         )
         # here the 7 is the length of '.tar.gz' string
-        with tarfile.open(pack_path_comp[:-7] + ".tar.gz", "r:gz") as tar:
+        with tarfile.open(pack_path_comp, "r:gz") as tar:
             tar.extractall()
         compare_obj = dircmp(pack_path_comp[:-7], pr_imp.path)
         self.assertEqual(len(compare_obj.diff_files), 0)
