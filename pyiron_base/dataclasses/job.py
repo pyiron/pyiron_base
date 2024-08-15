6c9d1615becd1c109a2e54970fd0ebedf2023877
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-08-15 16:28:45 +0200

    Update pyiron_base/dataclasses/job.py
    Co-authored-by: Marvin Poul <poul@mpie.de>

--- pyiron_base/dataclasses/job.py
+++ pyiron_base/dataclasses/job.py
@@ -4,9 +4,9 @@ from typing import List, Optional
 
 @dataclass
 class Executable:
-    version: Optional[str]
     name: str
     operation_system_nt: bool
-    executable: Optional[str]
     mpi: bool
     accepted_return_codes: List[int]
+    version: Optional[str] = None
+    executable: Optional[str] = None
