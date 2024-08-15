c06ede3ec041c758bb9d3bdef943681afff8f831
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-08-15 16:29:22 +0200

    Update pyiron_base/jobs/job/extension/executable.py
    Co-authored-by: Marvin Poul <poul@mpie.de>

--- pyiron_base/jobs/job/extension/executable.py
+++ pyiron_base/jobs/job/extension/executable.py
@@ -220,7 +220,12 @@ class Executable(HasDict):
             "mpi",
             "accepted_return_codes",
         ]
-        executable_class_dict = {}
+        executable_class_dict = {}        
+        # Backwards compatibility; dict state used to be nested one level deeper
+        if "executable" in executable_dict.keys() and isinstance(
+            executable_dict["executable"], dict
+        ):
+            executable_dict = executable_dict["executable"]
         for key in data_container_keys:
             executable_class_dict[key] = executable_dict.get(key, None)
 
