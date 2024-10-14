ed8bdc132a532495016fa2c8f1a4758e0b8a9b86
samwaseda <o.waseda@mpie.de>
2024-10-14 07:37:40 +0000

    unify pyiron_job and pyiron_job_simple so that it works with or without parentheses

--- pyiron_base/project/decorator.py
+++ pyiron_base/project/decorator.py
@@ -5,7 +5,10 @@ from pyiron_base.jobs.job.extension.server.generic import Server
 from pyiron_base.project.generic import Project
 
 
+# The combined decorator
 def pyiron_job(
+    funct: Optional[callable] = None,
+    *,
     project: Project = Project("."),
     host: Optional[str] = None,
     queue: Optional[str] = None,
@@ -17,10 +20,11 @@ def pyiron_job(
     output_file_lst: list = [],
     output_key_lst: list = [],
 ):
-    def pyiron_job_function(funct) -> callable:
+    # This is the actual decorator function that applies to the decorated function
+    def pyiron_job_function(f) -> callable:
         def function(*args, **kwargs):
             delayed_job_object = project.wrap_python_function(
-                python_function=funct,
+                python_function=f,
                 *args,
                 job_name=None,
                 automatically_rename=True,
@@ -40,43 +44,46 @@ def pyiron_job(
                 new_hdf=new_hdf,
             )
             return delayed_job_object
+        return function
+
+    # If funct is None, it means the decorator is called with arguments (like @pyiron_job(...))
+    if funct is None:
+        return pyiron_job_function
+
+    # If funct is not None, it means the decorator is called without parentheses (like @pyiron_job)
+    else:
+        # Assume this usage and handle the decorator like `pyiron_job_simple`
+        def function(
+            *args,
+            project: Project = Project("."),
+            resource_dict: dict = {},
+            output_file_lst: list = [],
+            output_key_lst: list = [],
+            **kwargs,
+        ):
+            resource_default_dict = {
+                k: v.default for k, v in inspect.signature(Server).parameters.items()
+            }
+            resource_dict.update(
+                {
+                    k: v
+                    for k, v in resource_default_dict.items()
+                    if k not in resource_dict.keys()
+                }
+            )
+            delayed_job_object = project.wrap_python_function(
+                python_function=funct,
+                *args,
+                job_name=None,
+                automatically_rename=True,
+                execute_job=False,
+                delayed=True,
+                output_file_lst=output_file_lst,
+                output_key_lst=output_key_lst,
+                **kwargs,
+            )
+            delayed_job_object._server = Server(**resource_dict)
+            return delayed_job_object
 
         return function
 
-    return pyiron_job_function
-
-
-def pyiron_job_simple(funct) -> callable:
-    def function(
-        *args,
-        project: Project = Project("."),
-        resource_dict: dict = {},
-        output_file_lst: list = [],
-        output_key_lst: list = [],
-        **kwargs,
-    ):
-        resource_default_dict = {
-            k: v.default for k, v in inspect.signature(Server).parameters.items()
-        }
-        resource_dict.update(
-            {
-                k: v
-                for k, v in resource_default_dict.items()
-                if k not in resource_dict.keys()
-            }
-        )
-        delayed_job_object = project.wrap_python_function(
-            python_function=funct,
-            *args,
-            job_name=None,
-            automatically_rename=True,
-            execute_job=False,
-            delayed=True,
-            output_file_lst=output_file_lst,
-            output_key_lst=output_key_lst,
-            **kwargs,
-        )
-        delayed_job_object._server = Server(**resource_dict)
-        return delayed_job_object
-
-    return function
--- tests/unit/flex/test_decorator.py
+++ tests/unit/flex/test_decorator.py
@@ -1,5 +1,6 @@
 from pyiron_base._tests import TestWithProject
-from pyiron_base import pyiron_job, pyiron_job_simple
+from pyiron_base import pyiron_job
+import unittest
 
 
 class TestPythonFunctionDecorator(TestWithProject):
@@ -23,11 +24,11 @@ class TestPythonFunctionDecorator(TestWithProject):
         self.assertEqual(len(edges_lst), 6)
 
     def test_delayed_simple(self):
-        @pyiron_job_simple
+        @pyiron_job
         def my_function_a(a, b=8):
             return a + b
 
-        @pyiron_job_simple
+        @pyiron_job
         def my_function_b(a, b=8):
             return a + b
 
@@ -37,3 +38,6 @@ class TestPythonFunctionDecorator(TestWithProject):
         nodes_dict, edges_lst = d.get_graph()
         self.assertEqual(len(nodes_dict), 6)
         self.assertEqual(len(edges_lst), 6)
+
+if __name__ == '__main__':
+    unittest.main()
