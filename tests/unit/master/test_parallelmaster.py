14042c7838de21d19e046ee0aae3f49151049271
Jan Janßen <janssen@mpie.de>
2025-08-16 21:44:46 +0200

    fixes

--- tests/unit/master/test_parallelmaster.py
+++ tests/unit/master/test_parallelmaster.py
@@ -2,7 +2,9 @@
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
+import pandas as pd
 import unittest
+from unittest.mock import patch, MagicMock
 from pyiron_base import JobGenerator, ParallelMaster
 from pyiron_base._tests import TestWithProject, ToyJob
 
